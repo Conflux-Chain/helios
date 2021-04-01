3b803d499609b2dbd1da4285aede03887b1e66d0
yqrashawn <namy.19@gmail.com>
2021-04-01 18:56:14 +0800

    feat(db): add delete one, extend Entity, support :db/isComponent

--- packages/db/src/main/cfxjs/db/core.cljs
+++ packages/db/src/main/cfxjs/db/core.cljs
@@ -1,6 +1,6 @@
 (ns cfxjs.db.core
   (:require [datascript.core :as d]
-            [datascript.impl.entity :refer [entity?]])
+            [cfxjs.db.impl.entity :refer [entity?] :as de])
   (:require-macros [cfxjs.db.core :refer [def-get-by-query def-get-query-or def-get-query-and def-get-one-query-and]]))
 
 (defn j->c [v]
@@ -11,15 +11,16 @@
 (def t (partial d/transact! conn))
 (def q (fn [query & args] (apply d/q query @conn args)))
 (def p (partial d/pull @conn))
-(defn e [& args] (apply d/entity @conn args))
+(defn e [& args] (apply de/entity @conn args))
 
-(defn- parse-attr-value [{:keys [ref many one doc identity value]}]
+(defn- parse-attr-value [{:keys [ref many one doc identity value component]}]
   (cond ref [:db/valueType :db.type/ref]
         many [:db/cardinality :db.cardinality/many]
         one [:db/cardinality :db.cardinality/one]
         doc [:db/doc doc]
         identity [:db/unique :db.unique/identity]
-        value [:db/unique :db.unique/value]))
+        value [:db/unique :db.unique/value]
+        component [:db/isComponent true]))
 
 (defn- js-schema->schema
   "Convert js schema format to datascript schema,
@@ -130,25 +131,38 @@
                   query (if or? (def-get-query-or query-attr-k symbols)
                             (def-get-query-and query-attr-k symbols))]
               (q query (mapv #(get % 2) data))))]
-    (comp clj->js #(map (comp entity->proxy e) %) f)))
+    f))
 
 (defn def-get-one-fn
   [{:keys [attrs model]}]
   (let [f (fn [attr-map]
             (let [attr-map (j->c attr-map)
                   data (filter vector? (mapv (fn [attr] (if (not (contains? attr-map attr))
-                                                          nil
-                                                          (let [symbol (->attr-symbol attr)
-                                                                query-attr-k (->attrk model attr)
-                                                                value (get attr-map attr)
-                                                                value (or (get-in value [:_entity :db/id]) value)]
-                                                            [symbol query-attr-k value])))
+                                                         nil
+                                                         (let [symbol (->attr-symbol attr)
+                                                               query-attr-k (->attrk model attr)
+                                                               value (get attr-map attr)
+                                                               value (or (get-in value [:_entity :db/id]) value)]
+                                                           [symbol query-attr-k value])))
                                              attrs))
                   symbols (mapv first data)
                   query-attr-k (mapv second data)
-                  query (def-get-one-query-and query-attr-k symbols)]
-              (q query (mapv #(get % 2) data))))]
-    (comp clj->js entity->proxy e f)))
+                  query (def-get-one-query-and query-attr-k symbols)
+                  rst (q query (mapv #(get % 2) data))]
+              rst))]
+    f))
+
+(defn def-delete-one-fn
+  [arg]
+  (let [f (comp
+           #(try
+              (and (t [[:db.fn/retractEntity %]]) true)
+              (catch js/Object e
+                ;; no need to write error to console when failed to delete
+                ;; (js/console.error e)
+                false))
+           (def-get-one-fn arg))]
+    f))
 
 (defn def-get-by-fn
   "Given model eg. :vault, attr eg. :type create the getVaultByType function"
@@ -156,7 +170,7 @@
   (let [attrk (->attrk model attr)
         query (def-get-by-query attrk)
         f (fn [attrv] (q query attrv))]
-    (comp clj->js #(map (comp entity->proxy e) %) f)))
+    f))
 
 (defn def-create-fn [{:keys [attrs model]}]
   (let [input-attr-map->transact-attr-map (fn [acc attr attrv]
@@ -178,18 +192,20 @@
         Model-name (str (.toUpperCase (subs model-name 0 1)) (subs model-name 1))
         attrs (map second struct)
         get-fn-k (keyword (str "get" Model-name))
-        get-fn (def-get-fn {:model model :attrs attrs})
+        get-fn (comp clj->js #(map (comp entity->proxy e) %) (def-get-fn {:model model :attrs attrs}))
         get-one-fn-k (keyword (str "getOne" Model-name))
-        get-one-fn (def-get-one-fn {:model model :attrs attrs})
+        get-one-fn (comp clj->js entity->proxy e (def-get-one-fn {:model model :attrs attrs}))
+        delete-one-fn-k (keyword (str "deleteOne" Model-name))
+        delete-one-fn (def-delete-one-fn {:model model :attrs attrs})
         create-fn-k (keyword (str "create" Model-name))
         create-fn (def-create-fn {:model model :attrs attrs})
         attr->attr-fn-map (fn [attr]
                             (let [attr-name (name attr)
                                   Attr-name (str (.toUpperCase (subs attr-name 0 1)) (subs attr-name 1))
                                   get-by-fn-k (keyword (str "get" Model-name "By" Attr-name))
-                                  get-by-fn (def-get-by-fn {:attr attr :model model})]
+                                  get-by-fn (comp clj->js #(map (comp entity->proxy e) %) (def-get-by-fn {:attr attr :model model}))]
                               {get-by-fn-k get-by-fn}))]
-    (apply merge {create-fn-k create-fn} {get-one-fn-k get-one-fn} {get-fn-k get-fn} (map attr->attr-fn-map attrs))))
+    (apply merge {delete-one-fn-k delete-one-fn} {create-fn-k create-fn} {get-one-fn-k get-one-fn} {get-fn-k get-fn} (map attr->attr-fn-map attrs))))
 
 (defn create-db
   "Create a database with js format schema, return a map with generated query methods for model and the database at :_db"
