998ea86d453bc3ce401b9b5b94c008395e68f009
yqrashawn <namy.19@gmail.com>
2021-04-12 17:57:23 +0800

    feat(vault): add cfxOnly to public vault
    if user giving a base32 address vault, mark it as cfx only vault, don't create
    ethHexAddress on the corresponding account when creating it from the vault
    
    For hex address public vault, store the original hex address. Create a normal
    account with ethHexAddress and convert it to 0x-prefixed address as cfxHexAddress.

--- packages/rpcs/wallet_addVault/index.js
+++ packages/rpcs/wallet_addVault/index.js
@@ -5,11 +5,12 @@ import {
   privateKey,
   base32AccountMainnetAddress,
   base32AccountTestnetAddress,
-  hexAccountAddress,
+  ethHexAddress,
   password,
 } from '@cfxjs/spec'
 import {encrypt, decrypt} from 'browser-passworder'
 import {partial, compL} from '@cfxjs/compose'
+import {validateBase32Address, decode} from '@cfxjs/base32-address'
 
 export const NAME = 'wallet_addVault'
 
@@ -22,7 +23,7 @@ const addressSchema = [
     'address',
     [
       or,
-      hexAccountAddress,
+      ethHexAddress,
       base32AccountMainnetAddress,
       base32AccountTestnetAddress,
     ],
@@ -38,6 +39,12 @@ export const permissions = {
   db: ['createVault'],
 }
 
+const processAddress = address => {
+  const isBase32 = validateBase32Address(address)
+  if (!isBase32) return {address, cfxOnly: false}
+  return {cfxOnly: true, address: decode(address).hexAddress}
+}
+
 export async function main({
   db: {createVault},
   params: {password, mnemonic, privateKey, address},
@@ -46,24 +53,33 @@ export async function main({
 }) {
   if (!(await wallet_validatePassword({password})))
     throw Err.InvalidParams('Invalid password')
-  const keyring = mnemonic || privateKey || address
-  let keyringType
-  if (address) keyringType = 'pub'
-  if (privateKey) keyringType = 'pk'
-  if (mnemonic) keyringType = 'hd'
-  const encrypted = await encrypt(password, keyring)
+
+  const vault = {}
+  vault.data = mnemonic || privateKey || address
+  if (privateKey) vault.type = 'pk'
+  else if (mnemonic) vault.type = 'hd'
+  else if (address) {
+    const validateResult = processAddress(address, Err)
+    vault.type = 'pub'
+    vault.data = validateResult.address
+    vault.cfxOnly = validateResult.cfxOnly
+  }
+
   const vaults = await wallet_getVaults()
   const anyDuplicateVaults = await Promise.all(
     vaults.map(
       compL(
         v => v.data,
         partial(decrypt, password), // decrypt vault, returns stringified { data:..., type:...}
-        p => p.then(data => data === keyring),
+        p => p.then(data => data === vault.data),
       ),
     ),
   )
+
   if (anyDuplicateVaults.includes(true))
     throw Err.InvalidParams('Duplicate credential')
 
-  createVault({data: encrypted, type: keyringType})
+  vault.data = await encrypt(password, vault.data)
+
+  createVault(vault)
 }
