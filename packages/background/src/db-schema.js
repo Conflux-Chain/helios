690ce1fb33cd4d0f7b458e9af75c2e4262588de5
yqrashawn <namy.19@gmail.com>
2021-04-12 18:48:16 +0800

    feat(vault): add cfxOnly to public vault (#19)

--- packages/background/src/db-schema.js
+++ packages/background/src/db-schema.js
@@ -6,6 +6,9 @@ const schema = {
     data: {
       doc: 'Encrypted vault data',
     },
+    cfxOnly: {
+      doc: 'If this vault is only for conflux chain',
+    },
     accounts: {
       doc: 'Accounts belong to this vault',
       many: true,
