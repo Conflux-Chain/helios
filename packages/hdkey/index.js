d76a6b32b409e9f0767dd32b7911b1a1c20e46e8
yqrashawn <namy.19@gmail.com>
2021-04-13 16:50:19 +0800

    fix(hdkey): index error, make it async function

--- packages/hdkey/index.js
+++ packages/hdkey/index.js
@@ -1,30 +1,32 @@
-import {mnemonicToSeedSync} from 'bip39'
+import {mnemonicToSeed} from 'bip39'
 import {hdkey} from 'ethereumjs-wallet'
 
 const DEFAULT_HD_PATH = `m/44'/503'/0'/0`
 
-export const defHDKey = (mnemonic, hdpath = DEFAULT_HD_PATH) =>
-  hdkey.fromMasterSeed(mnemonicToSeedSync(mnemonic)).derivePath(hdpath)
+export const defHDKey = async (mnemonic, hdPath = DEFAULT_HD_PATH) => {
+  const seed = await mnemonicToSeed(mnemonic)
+  return hdkey.fromMasterSeed(seed).derivePath(hdPath)
+}
 
-export const getNthAccountOfHDKey = ({
+export const getNthAccountOfHDKey = async ({
   mnemonic,
-  hdpath,
+  hdPath,
   nth,
   only0x1Prefixed = false,
 }) => {
-  const k = defHDKey(mnemonic, hdpath)
+  const k = await defHDKey(mnemonic, hdPath)
   const result = {}
   let count = 0,
     idx = 0
 
   while (count <= nth) {
-    const newWallet = k.deriveChild(++idx).getWallet()
+    const newWallet = k.deriveChild(idx++).getWallet()
     result.address = '0x' + newWallet.getAddress().toString('hex')
     result.privateKey = '0x' + newWallet.getPrivateKey().toString('hex')
     if (only0x1Prefixed && result.address.startsWith('0x1')) count++
     if (!only0x1Prefixed) count++
   }
-  result.index = count - 1
+  result.index = idx - 1
 
   return result
 }
