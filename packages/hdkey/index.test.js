cc080803e208fba9c84573d3b6c2a2dce78758e6
yqrashawn <namy.19@gmail.com>
2021-04-13 17:06:06 +0800

    fix(hdkey): index error, make it async function (#21)

--- packages/hdkey/index.test.js
+++ packages/hdkey/index.test.js
@@ -5,7 +5,7 @@ import {generateMnemonic} from 'bip39'
 
 describe('HDKey', function () {
   it('should return a hdkey', async function () {
-    const account = getNthAccountOfHDKey({
+    const account = await getNthAccountOfHDKey({
       mnemonic: generateMnemonic(),
       nth: 1,
       only0x1Prefixed: true,
@@ -13,5 +13,17 @@ describe('HDKey', function () {
 
     expect(account).toBeDefined()
     expect(account.index >= 1).toBe(true)
+  })
+
+  it('should return a the right address', async function () {
+    const account = await getNthAccountOfHDKey({
+      mnemonic:
+        'error mom brown point sun magnet armor fish urge business until plastic',
+      nth: 0,
+    })
+
+    expect(account.address.toLowerCase()).toBe(
+      '0x7b3d01a14c84181f4df3983ae68118e4bad48407',
+    )
   })
 })
