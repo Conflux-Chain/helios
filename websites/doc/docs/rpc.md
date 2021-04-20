99aced3b1265f5f6e5e8fd87bcebdafc946b1704
yqrashawn <namy.19@gmail.com>
2021-04-20 22:08:33 +0800

    feat(doc): add docusaurus

---
+++ websites/doc/docs/rpc.md
@@ -0,0 +1,7 @@
+---
+id: rpc
+title: RPC
+custom_edit_url: https://github.com/Conflux-Chain/helios/edit/dev/websites/doc/docs/rpc.md
+---
+
+# RPC
---
+++ websites/doc/src/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout' // eslint-disable-line import/no-unresolved
+import Link from '@docusaurus/Link' // eslint-disable-line import/no-unresolved
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext' // eslint-disable-line import/no-unresolved
+import styles from './index.module.css'
+import HomepageFeatures from '../components/HomepageFeatures'
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext()
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="/docs">
+            Docusaurus Tutorial - 5min
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext()
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  )
+}
