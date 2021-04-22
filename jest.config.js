d07df815efa9466380e93e5ba3710a6d1a07c617
yqrashawn <namy.19@gmail.com>
2021-04-22 16:29:21 +0800

    test: coverage report setting

--- jest.config.js
+++ jest.config.js
@@ -78,6 +78,7 @@ module.exports = {
     '/node_modules/',
     'index.dev.js',
     'index.prod.js',
+    '/websites/doc',
     '/packages/browser-extension',
     '/packages/csp',
     '/packages/consts',
@@ -88,6 +89,7 @@ module.exports = {
     '/packages/bytes/',
     '/testlib/',
     '/packages/storybook',
+    '/packages/workspace-tools',
   ],
 
   // The directory where Jest should output its coverage files
