8919ae95f53a4517f64b33d4dc93933129a62ac2
yqrashawn <namy.19@gmail.com>
2021-05-28 16:43:28 +0800

    feat(rpcs): add cfx_getCode eth_getCode

---
+++ packages/rpcs/cfx_getCode/index.js
@@ -0,0 +1,17 @@
+import {base32ContractAddress, cat, epochRef} from '@cfxjs/spec'
+
+export const NAME = 'cfx_getCode'
+
+export const cache = {
+  type: 'epoch',
+  ttl: 3600,
+  key: ({params}) => `${NAME}${params[0]}`,
+}
+
+export const schemas = {
+  input: [cat, base32ContractAddress, epochRef],
+}
+
+export const main = async ({f, params}) => {
+  return await f(params)
+}
---
+++ packages/rpcs/eth_getCode/index.js
@@ -0,0 +1,17 @@
+import {ethHexAddress, cat, blockTag} from '@cfxjs/spec'
+
+export const NAME = 'eth_getCode'
+
+export const schemas = {
+  input: [cat, ethHexAddress, blockTag],
+}
+
+export const cache = {
+  type: 'block',
+  ttl: 3600,
+  key: ({params}) => `${NAME}${params[0]}`,
+}
+
+export const main = async ({f, params}) => {
+  return await f(params)
+}
