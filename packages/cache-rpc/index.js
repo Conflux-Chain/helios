a8298074786470519432ca79873501b8ba931a33
yqrashawn <namy.19@gmail.com>
2021-06-24 20:37:17 +0800

    feat: add before/after set/get interface to cache-rpc

--- packages/cache-rpc/index.js
+++ packages/cache-rpc/index.js
@@ -2,7 +2,7 @@ import {isArray, isFunction, isNumber, isObject, isString} from '@cfxjs/checks'
 import {TLRUCache, LRUCache} from '@thi.ng/cache'
 import EpochRefConf from '@cfxjs/rpc-epoch-ref'
 
-const CACHE = {}
+let CACHE = {}
 
 export const getCacheStore = (
   {networkName, network, params, method},
@@ -49,6 +49,8 @@ export const getCacheKey = (key, req) => {
 }
 
 export const getCache = ({req, conf}) => {
+  if (isFunction(conf?.beforeGet) && !conf.beforeGet(req)) return
+
   if (!conf || !conf.type || !conf.key) return
   const {key} = conf
 
@@ -56,10 +58,17 @@ export const getCache = ({req, conf}) => {
   if (!Cache) return
 
   const k = getCacheKey(key, req)
-  return Cache.get(k)
+
+  let c = Cache.get(k)
+
+  if (isFunction(conf?.afterGet)) c = conf.afterGet(req, c)
+
+  return c
 }
 
 export const setCache = ({req, res, conf}) => {
+  if (isFunction(conf?.beforeSet) && !conf.beforeSet(req, res)) return
+
   if (!conf || !conf.type || !conf.key || !res || !res.result) return
   const {ttl, key} = conf
 
@@ -68,10 +77,13 @@ export const setCache = ({req, res, conf}) => {
 
   const k = getCacheKey(key, req)
   Cache.set(k, res.result, ttl)
+
+  if (isFunction(conf?.afterSet)) conf.afterSet(setCache, req, res)
 }
 
 export const init = () =>
   // config = {}
   {
+    CACHE = {}
     return {get: getCache, set: setCache}
   }
