d212da0a55e855b12fc6546d3416ac8c0e232940
yqrashawn <namy.19@gmail.com>
2021-07-01 16:28:02 +0800

    feat: add cache for eth_blockNumber

--- packages/rpcs/eth_blockNumber/index.js
+++ packages/rpcs/eth_blockNumber/index.js
@@ -1,12 +1,21 @@
+import {and, arrp, empty} from '@cfxjs/spec'
+
 export const NAME = 'eth_blockNumber'
 
-export const schemas = {}
+export const schemas = {
+  input: [and, arrp, empty],
+}
 
 export const permissions = {
   locked: true,
   external: ['popup', 'inpage'],
 }
 
-export const main = async ({f}) => {
-  return await f()
+export const cache = {
+  type: 'ttl',
+  ttl: 1000, // TODO: block time of each eth-type network is different
+}
+
+export const main = async ({f, params}) => {
+  return await f(params)
 }
