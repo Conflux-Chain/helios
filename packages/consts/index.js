ec25f885a3814371968f378ba3104caf2068a940
yqrashawn <namy.19@gmail.com>
2021-07-01 16:27:16 +0800

    feat: use hex string for chainid, number for netid

--- packages/consts/index.js
+++ packages/consts/index.js
@@ -26,13 +26,13 @@ export const ADDRESS_TYPES = ['user', 'contract', 'builtin', 'null']
 // * network setting
 export const CFX_MAINNET_RPC_ENDPOINT = 'https://portal-main.confluxrpc.com'
 export const CFX_MAINNET_NAME = 'CFX_MAINNET'
-export const CFX_MAINNET_CHAINID = 1029
+export const CFX_MAINNET_CHAINID = '0x405'
 export const CFX_MAINNET_NETID = 1029
 export const CFX_MAINNET_CURRENCY_SYMBOL = 'CFX'
 
 export const CFX_TESTNET_RPC_ENDPOINT = 'https://portal-test.confluxrpc.com'
 export const CFX_TESTNET_NAME = 'CFX_TESTNET'
-export const CFX_TESTNET_CHAINID = 1
+export const CFX_TESTNET_CHAINID = '0x1'
 export const CFX_TESTNET_NETID = 1
 export const CFX_TESTNET_CURRENCY_SYMBOL = 'CFX'
 
@@ -40,27 +40,27 @@ export const CFX_TESTNET_CURRENCY_SYMBOL = 'CFX'
 export const ETH_MAINNET_RPC_ENDPOINT =
   'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
 export const ETH_MAINNET_NAME = 'ETH_MAINNET'
-export const ETH_MAINNET_CHAINID = 1
+export const ETH_MAINNET_CHAINID = '0x1'
 export const ETH_MAINNET_NETID = 1
 export const ETH_MAINNET_CURRENCY_SYMBOL = 'ETH'
 
 export const ETH_ROPSTEN_RPC_ENDPOINT =
   'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
 export const ETH_ROPSTEN_NAME = 'ETH_ROPSTEN'
-export const ETH_ROPSTEN_CHAINID = 3
+export const ETH_ROPSTEN_CHAINID = '0x3'
 export const ETH_ROPSTEN_NETID = 3
 export const ETH_ROPSTEN_CURRENCY_SYMBOL = 'ETH'
 
 export const BSC_MAINNET_RPC_ENDPOINT = 'https://bsc-dataseed.binance.org/'
 export const BSC_MAINNET_NAME = 'BSC_MAINNET'
-export const BSC_MAINNET_CHAINID = 56
+export const BSC_MAINNET_CHAINID = '0x38'
 export const BSC_MAINNET_NETID = 56
 export const BSC_MAINNET_CURRENCY_SYMBOL = 'BNB'
 
 export const BSC_TESTNET_RPC_ENDPOINT =
   'https://data-seed-prebsc-1-s1.binance.org:8545/'
 export const BSC_TESTNET_NAME = 'BSC_TESTNET'
-export const BSC_TESTNET_CHAINID = 97
+export const BSC_TESTNET_CHAINID = '0x61'
 export const BSC_TESTNET_NETID = 97
 export const BSC_TESTNET_CURRENCY_SYMBOL = 'BNB'
 
@@ -71,13 +71,13 @@ export const REGENERATE = 'REGENERATE'
 
 export const CFX_LOCALNET_RPC_ENDPOINT = 'http://localhost:12537'
 export const CFX_LOCALNET_NAME = 'CFX_LOCALNET'
-export const CFX_LOCALNET_CHAINID = 2999
+export const CFX_LOCALNET_CHAINID = '0xbb7'
 export const CFX_LOCALNET_NETID = 2999
 export const CFX_LOCALNET_CURRENCY_SYMBOL = 'CFX'
 
 export const ETH_LOCALNET_RPC_ENDPOINT = 'http://localhost:8545'
 export const ETH_LOCALNET_NAME = 'ETH_LOCALNET'
-export const ETH_LOCALNET_CHAINID = 1
+export const ETH_LOCALNET_CHAINID = '0x1'
 export const ETH_LOCALNET_NETID = 1337
 export const ETH_LOCALNET_CURRENCY_SYMBOL = 'ETH'
 
