0975c6826400009266bb1add39988ddb6cfa62fa
RickStormBringer <13621099447@163.com>
2021-08-20 10:19:39 +0800

    feat: add react-use

--- packages/popup/src/pages/b.js
+++ packages/popup/src/pages/b.js
@@ -1,3 +1,20 @@
-const b = () => <div>i am module bbbbbb</div>
+import {useStore} from '../zustlandDemo'
+function BearCounter() {
+  const bears = useStore(state => state.bears)
+  return <h1>{bears} around here ...</h1>
+}
+
+function Controls() {
+  const increasePopulation = useStore(state => state.increasePopulation)
+  return <button onClick={increasePopulation}>one up</button>
+}
+const b = () => {
+  return (
+    <div>
+      <BearCounter />
+      <Controls />
+    </div>
+  )
+}
 
 export default b
