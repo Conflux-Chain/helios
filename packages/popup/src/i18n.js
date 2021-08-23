38824d27066db2e11b5170602ca7396ec1416088
RickStormBringer <13621099447@163.com>
2021-08-23 19:15:39 +0800

    feat: remove backend

--- packages/popup/src/i18n.js
+++ packages/popup/src/i18n.js
@@ -2,25 +2,21 @@
 /* eslint-disable no-unused-vars */
 import i18next from 'i18next'
 import {initReactI18next} from 'react-i18next'
-import Backend from 'i18next-http-backend'
 import resources from './locales/index'
 
 const zhLanguages = ['zh', 'zh-CN', 'zh-TW', 'zh-HK']
-i18next
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    resources,
-    react: {
-      useSuspense: true,
-    },
-    debug: import.meta.env.NODE_ENV === 'development' ? true : false,
-    lng:
-      navigator?.language && zhLanguages.includes(navigator.language)
-        ? 'zh'
-        : 'en',
-    fallbackLng: 'en',
-    preload: ['en'],
-  })
+i18next.use(initReactI18next).init({
+  resources,
+  react: {
+    useSuspense: true,
+  },
+  debug: import.meta.env.NODE_ENV === 'development' ? true : false,
+  lng:
+    navigator?.language && zhLanguages.includes(navigator.language)
+      ? 'zh'
+      : 'en',
+  fallbackLng: 'en',
+  preload: ['en'],
+})
 
 export default i18next
