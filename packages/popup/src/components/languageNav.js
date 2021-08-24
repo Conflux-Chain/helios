272bfc4642457528c8d08c2daab34328717cde35
RickStormBringer <13621099447@163.com>
2021-08-24 11:28:00 +0800

    feat: add languageNav.js

--- packages/popup/src/components/languageNav.js
+++ packages/popup/src/components/languageNav.js
@@ -1,12 +1,23 @@
 import Dropdown from '@cfxjs/component-dropdown'
-import {Languages} from '../constants'
 import {useTranslation} from 'react-i18next'
+import {Languages} from '../constants'
+
+const Overlay = changeLanguage => {
+  return (
+    <>
+      {Languages.map(lang => (
+        <div aria-hidden="true" key={lang} onClick={changeLanguage}>
+          {lang}
+        </div>
+      ))}
+    </>
+  )
+}
 
 const LanguageNav = () => {
   const {i18n} = useTranslation()
   const {language} = i18n
-
-  const onChangeLanguage = () => {
+  const changeLanguage = () => {
     if (language.indexOf('en') !== -1) {
       i18n.changeLanguage('zh-CN')
     } else if (language.indexOf('zh') !== -1) {
@@ -14,15 +25,9 @@ const LanguageNav = () => {
     }
   }
   return (
-    <header>
-      <Dropdown overlay={<div>test menu</div>}>
-        <>
-          {Languages.map(lang => (
-            <div aria-hidden="true" key={lang} onClick={onChangeLanguage}>
-              {lang}
-            </div>
-          ))}
-        </>
+    <header className="text-right">
+      <Dropdown overlay={Overlay(changeLanguage)} trigger={['click']}>
+        <span>{language}</span>
       </Dropdown>
     </header>
   )
