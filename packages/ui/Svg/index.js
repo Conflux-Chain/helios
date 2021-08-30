b3687070b801a7186ed266568df6f0d186c72104
angelia <greenfish84@126.com>
2021-08-30 17:53:51 +0800

    feat: add the ui components folder and icons folder

--- packages/ui/Svg/index.js
+++
@@ -1,2 +0,0 @@
-export {default as EyeClose} from './eyeClose'
-export {default as EyeOpen} from './eyeOpen'
---
+++ packages/ui/icons/index.js
@@ -0,0 +1,2 @@
+export {default as EyeClose} from './EyeClose'
+export {default as EyeOpen} from './EyeOpen'
