3f97c4ffbb9b6c05da0a10e4d24fc113cb417a55
yqrashawn <namy.19@gmail.com>
2021-09-02 16:20:44 +0800

    feat: register site metadata on inpage load

--- packages/content-script/index.js
+++ packages/content-script/index.js
@@ -1,8 +1,27 @@
 import browser from 'webextension-polyfill'
 import {stream} from '@thi.ng/rstream'
+import {getSiteMetadata} from '@cfxjs/site-metadata'
+
+let CONNECT_RETRY_COUNT = 0
+let INPAGE_INJECTED = false
+let s
+
+// need to call this after inpage.js is injected
+// so that the dapp page can get the connected event
+function registerSite() {
+  if (!s) return
+  getSiteMetadata().then(metadata => {
+    if (!metadata.icon) delete metadata.icon
+    s.next.call(s, {
+      method: 'wallet_registerSiteMetadata',
+      params: metadata,
+      _origin: location.host,
+    })
+  })
+}
 
 function setup() {
-  const s = stream({
+  s = stream({
     id: 'content-script',
     closeIn: false,
     closeOut: false,
@@ -14,6 +33,7 @@ function setup() {
   s.subscribe(sub)
 
   const listenToInpageMessage = e => {
+    CONNECT_RETRY_COUNT = 0
     if (e.origin !== location.origin) return
     if (e.source !== window) return
     if (
@@ -26,14 +46,32 @@ function setup() {
     if (!e.data.msg.method) return
     if (e.data.msg.jsonrpc !== '2.0') return
     if (!Number.isInteger(e.data.msg.id)) return
+    if (e.data.msg.method === 'wallet_registerSiteMetadata') return
     s.next.call(s, {...e.data.msg, _origin: location.host})
   }
 
   window.addEventListener('message', listenToInpageMessage, false)
 
   port.onDisconnect.addListener(() => {
+    window.postMessage({
+      msg: {
+        event: 'disconnect',
+        params: {
+          code: 4900,
+          message:
+            "Can't connect to extension runtime, disconnected from all chain. Please refresh the page or tell user to refresh the page.",
+        },
+      },
+    })
     window.removeEventListener('message', listenToInpageMessage)
     s.unsubscribe(sub)
+    s = null
+    if (CONNECT_RETRY_COUNT >= 10) {
+      return
+    }
+    console.info(
+      `Failed to connect background, retry: ${++CONNECT_RETRY_COUNT}`,
+    )
     setup()
   })
 
@@ -43,15 +81,20 @@ function setup() {
       location.origin,
     )
   })
+
+  if (INPAGE_INJECTED) registerSite()
 }
 
 setup()
 
 function injectInpage() {
+  window.removeEventListener('DOMContentLoaded', injectInpage)
   const inpage = document.createElement('script')
   inpage.src = browser.runtime.getURL('inpage.js')
   inpage.async = false
   document.head.appendChild(inpage)
+  INPAGE_INJECTED = true
+  registerSite()
 }
 
 window.addEventListener('DOMContentLoaded', injectInpage)
