65316e2325354c850d17e0bf6e25d7a9c6bd8b78
yqrashawn <namy.19@gmail.com>
2021-09-02 17:04:43 +0800

    feat: add network/account logic/rpc/middleware

---
+++ packages/rpcs/wallet_addEthereumChain/index.js
@@ -0,0 +1,85 @@
+import {cat, map, or, dbid} from '@cfxjs/spec'
+import {ChainParameterSchema} from '@cfxjs/wallet_add-network'
+
+export const NAME = 'wallet_addEthereumChain'
+
+const PublicSchema = [cat, ChainParameterSchema]
+const InnerSchema = [
+  map,
+  {closed: true},
+  ['authReqId', dbid],
+  ['newChainConfig', PublicSchema],
+]
+export const schemas = {
+  input: [or, PublicSchema, InnerSchema],
+}
+
+export const permissions = {
+  external: ['inpage', 'popup'],
+  methods: [
+    'wallet_detectNetworkType',
+    'wallet_addNetwork',
+    'wallet_addPendingUserAuthRequest',
+    'wallet_userApprovedAuthRequest',
+    'wallet_userRejectedAuthRequest',
+  ],
+  db: ['getNetwork', 'getAuthReqById', 'getNetworkByEndpoint'],
+  scope: {wallet_networks: {}},
+}
+
+export const main = async ({
+  Err: {InvalidParams},
+  db: {getAuthReqById, getNetworkByEndpoint, getNetwork},
+  rpcs: {
+    wallet_detectNetworkType,
+    wallet_addNetwork,
+    wallet_addPendingUserAuthRequest,
+    wallet_userApprovedAuthRequest,
+    wallet_userRejectedAuthRequest,
+  },
+  app,
+  params,
+  _popup,
+  _inpage,
+}) => {
+  const chainConf = Array.isArray(params) ? params[0] : params.newChainConfig[0]
+  const {
+    chainId,
+    rpcUrls: [rpcUrl],
+  } = chainConf
+  const [dupEndpointNetwork] = getNetworkByEndpoint(rpcUrl)
+  if (dupEndpointNetwork)
+    throw InvalidParams(
+      `Duplicate network endpoint with network ${dupEndpointNetwork.eid}`,
+    )
+
+  const {chainId: detectedChainId, type: detectedNetworkType} =
+    await wallet_detectNetworkType({url: rpcUrl})
+  if (chainId !== detectedChainId)
+    throw InvalidParams(
+      `Invalid chainId ${chainId}, got ${detectedChainId} from remote`,
+    )
+
+  const [dupChainIdBuiltInNetwork] = getNetwork({
+    chainId,
+    builtin: true,
+    type: detectedNetworkType,
+  })
+  if (dupChainIdBuiltInNetwork)
+    throw InvalidParams(`Duplicate chainId ${chainId} with builtin network`)
+
+  if (_inpage) {
+    const req = {method: NAME, params}
+    return await wallet_addPendingUserAuthRequest({appId: app.eid, req})
+  }
+
+  if (_popup) {
+    const {authReqId} = params
+    if (!authReqId) throw InvalidParams(`Invalid auth req id ${authReqId}`)
+    const authReq = getAuthReqById(authReqId)
+    if (!authReq) throw InvalidParams(`Invalid auth req id ${authReqId}`)
+    const rst = await wallet_addNetwork(chainConf)
+    if (rst?.error) return await wallet_userRejectedAuthRequest({authReqId})
+    return await wallet_userApprovedAuthRequest({authReqId, res: null})
+  }
+}
