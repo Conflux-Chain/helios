ebc8cdc6fb29704d5133230250309924608be024
angelia <greenfish84@126.com>
2021-09-06 16:38:28 +0800

    feat: remove useless code

--- packages/popup/src/utils/index.js
+++ packages/popup/src/utils/index.js
@@ -15,7 +15,6 @@ export function getFreeList(arr) {
   let arrAdd = [...arr]
   for (let i = 1; i < arrAdd.length; i++) {
     const random = Math.floor(Math.random() * (i + 1))
-    //交换两个数组
     ;[arrAdd[i], arrAdd[random]] = [arrAdd[random], arrAdd[i]]
   }
   return arrAdd
