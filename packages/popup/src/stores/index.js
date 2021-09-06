96f0a730138cd09f29738d528e67895ebfa1e12c
RickStormBringer <13621099447@163.com>
2021-09-06 14:00:59 +0800

    feat: remove homeTitle and languageNav

--- packages/popup/src/stores/index.js
+++ packages/popup/src/stores/index.js
@@ -1,28 +1,5 @@
 import create from 'zustand'
 
-// let GlobalStore = null
-
-// const createGlobalStore = () =>
-//   create(set => ({
-//     // value
-//     createdGroupName: '',
-//     createdSeedPhase: '',
-//     createdPassword: '',
-
-//     // logic
-//     setCreatedPassword: createdPassword => set({createdPassword}),
-//     setCreatedGroupName: createdGroupName => set({createdGroupName}),
-//     setCreatedSeedPhase: createdSeedPhase => set({createdSeedPhase}),
-//   }))
-
-// const useGlobalStore = () => {
-//   if (!GlobalStore) GlobalStore = createGlobalStore()
-
-//   return GlobalStore
-// }
-
-// export default useGlobalStore
-
 const useGlobalStore = create(set => ({
   // value
   createdGroupName: '',
