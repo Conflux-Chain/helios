44ca51631e9e5c8328f6fec52eb522f0924405d0
RickStormBringer <13621099447@163.com>
2021-09-27 15:49:06 +0800

    test: update snapshot

--- packages/ui/components/Input/index.test.js
+++ packages/ui/components/Input/index.test.js
@@ -13,16 +13,16 @@ describe('Input', () => {
     expect(screen.getByTestId('input-text')).toHaveValue('test value')
   })
   it('should render incoming class name on input', () => {
-    render(<Input className="test-input" />)
+    render(<Input className="test-input" value="test value" />)
     expect(screen.getByTestId('input-text')).toHaveClass('test-input')
   })
   it('should render incoming container class name on input', () => {
-    render(<Input containerClassName="test-container" />)
+    render(<Input containerClassName="test-container" value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('test-container')
   })
   it('should trigger the incoming onChange function', () => {
     const onChange = jest.fn()
-    render(<Input onChange={onChange} />)
+    render(<Input onChange={onChange} value="test value" />)
     fireEvent.change(screen.getByTestId('input-text'), {
       target: {value: 'value'},
     })
@@ -30,57 +30,57 @@ describe('Input', () => {
   })
 
   it('test incoming width class name', () => {
-    render(<Input width="test-width" />)
+    render(<Input width="test-width" value="test value" />)
     expect(screen.getByTestId('input-wrapper')).toHaveClass('test-width')
   })
   it('test incoming small size', () => {
-    render(<Input size="small" />)
+    render(<Input size="small" value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('h-8')
   })
 
   it('test incoming medium size', () => {
-    render(<Input size="medium" />)
+    render(<Input size="medium" value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('h-10')
   })
 
   it('test incoming large size', () => {
-    render(<Input size="large" />)
+    render(<Input size="large" value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('h-12')
   })
   it('test incoming errorMessage', () => {
-    render(<Input errorMessage="error message" />)
+    render(<Input errorMessage="error message" value="test value" />)
     expect(screen.getByText('error message')).toBeInTheDocument()
   })
   it('test incoming prefix', () => {
-    render(<Input prefix="some label" />)
+    render(<Input prefix="some label" value="test value" />)
     expect(screen.getByText('some label')).toBeInTheDocument()
   })
   it('test incoming suffix', () => {
-    render(<Input suffix="suffix" />)
+    render(<Input suffix="suffix" value="test value" />)
     expect(screen.getByText('suffix')).toBeInTheDocument()
   })
   it('render disabled style', () => {
-    render(<Input disabled={true} />)
+    render(<Input disabled={true} value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass(
       'bg-gray-10 cursor-not-allowed',
     )
   })
   it('render regular style', () => {
-    render(<Input disabled={false} />)
+    render(<Input disabled={false} value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('bg-gray-0')
   })
   it('test border style when passing false bordered param', () => {
-    render(<Input bordered={false} />)
+    render(<Input bordered={false} value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass('border-0')
   })
   it('test border style when passing bordered param and errorMessage', () => {
-    render(<Input errorMessage="err msg" bordered={true} />)
+    render(<Input errorMessage="err msg" bordered={true} value="test value" />)
     expect(screen.getByTestId('input-container')).toHaveClass(
       'border-error border',
     )
   })
   it('test border style when passing bordered when focus and blur', () => {
-    render(<Input bordered={true} />)
+    render(<Input bordered={true} value="test value" />)
     fireEvent.focus(screen.getByTestId('input-text'))
     expect(screen.getByTestId('input-container')).toHaveClass('border-primary')
     fireEvent.blur(screen.getByTestId('input-text'))
@@ -88,7 +88,7 @@ describe('Input', () => {
   })
   it('test incoming blur function', () => {
     const onBlur = jest.fn()
-    render(<Input bordered={true} onBlur={onBlur} />)
+    render(<Input bordered={true} onBlur={onBlur} value="test value" />)
     fireEvent.focus(screen.getByTestId('input-text'))
     fireEvent.blur(screen.getByTestId('input-text'))
     expect(onBlur).toHaveBeenCalledTimes(1)
