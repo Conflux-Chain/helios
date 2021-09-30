08abc4f074261e831e0a471a0bc24abf663cec8a
RickStormBringer <13621099447@163.com>
2021-09-30 16:20:41 +0800

    feat: add balance

--- packages/popup/src/hooks/index.js
+++ packages/popup/src/hooks/index.js
@@ -1,9 +1,14 @@
 import {useEffect, useState} from 'react'
 import useGlobalStore from '../stores'
 import {useHistory, useLocation} from 'react-router-dom'
-import {ROUTES, ANIMATE_DURING_TIME} from '../constants'
+import {ROUTES, ANIMATE_DURING_TIME, RPC_METHODS} from '../constants'
+import {useRPC} from '@fluent-wallet/use-rpc'
+import {isNumber} from '@fluent-wallet/checks'
 
+const {GET_ACCOUNT_GROUP, GET_ACCOUNT_ADDRESS_BY_NETWORK, GET_BALANCE} =
+  RPC_METHODS
 const {HOME} = ROUTES
+
 export const useCreatedPasswordGuard = () => {
   const createdPassword = useGlobalStore(state => state.createdPassword)
   const history = useHistory()
@@ -49,4 +54,91 @@ export const useFontSize = (targetRef, hiddenRef, maxWidth, value) => {
       targetDom.style.fontSize = '14px'
     }
   }, [targetRef, hiddenRef, maxWidth, value])
+}
+
+const getAddressParams = (accountGroups, networkId) => {
+  let addressParams = []
+  if (isNumber(networkId) && accountGroups?.length) {
+    addressParams = accountGroups.reduce(
+      (acc, cur) =>
+        acc.concat(
+          cur.account
+            ? cur.account.map(({eid: accountId}) => ({networkId, accountId}))
+            : [],
+        ),
+      [],
+    )
+  }
+  return addressParams
+}
+
+const getBatchAddress = ({
+  groupIndex,
+  accountIndex,
+  accountGroups,
+  addressData,
+}) => {
+  let index
+  if (groupIndex == 0) {
+    index = accountIndex
+  } else {
+    index = accountGroups[groupIndex - 1].account.length + accountIndex
+  }
+  return addressData[index].base32 || addressData[index].hex
+}
+
+const getBatchBalanceData = ({accountGroups, balanceData, addressData}) => {
+  let ret = []
+  if (
+    accountGroups.length &&
+    addressData.length &&
+    Object.keys(balanceData).length
+  ) {
+    accountGroups.forEach(({nickname, account}, groupIndex) => {
+      ret.push({nickname, account: []})
+      account.forEach(({nickname, eid}, accountIndex) => {
+        const address = getBatchAddress({
+          groupIndex,
+          accountIndex,
+          accountGroups,
+          addressData,
+        })
+        ret[groupIndex]['account'].push({
+          nickname,
+          eid,
+          balance: window.BigInt(balanceData[address]['0x0']).toString(),
+        })
+      })
+    })
+  }
+  return ret
+}
+
+export const useAccountGroupBatchBalance = networkId => {
+  const {data: accountGroups} = useRPC([GET_ACCOUNT_GROUP], undefined)
+  const addressParams = getAddressParams(accountGroups, networkId)
+
+  // TODO: should mutate when add network
+  const {data: addressData} = useRPC(
+    addressParams.length && isNumber(networkId)
+      ? [GET_ACCOUNT_ADDRESS_BY_NETWORK, networkId]
+      : null,
+    addressParams,
+    {fallbackData: []},
+  )
+
+  const {data: balanceData} = useRPC(
+    addressData.length ? [GET_BALANCE, networkId] : null,
+    {
+      users: addressData.map(data => data.base32 || data.hex),
+      tokens: ['0x0'],
+    },
+    {fallbackData: {}},
+  )
+
+  return getBatchBalanceData({
+    accountGroups,
+    balanceData,
+    addressData,
+  })
 }
