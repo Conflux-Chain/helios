f902256047630b22f87c8e5ce7b72554f68fb70b
RickStormBringer <13621099447@163.com>
2021-11-18 18:43:11 +0800

    feat: add global error configuration

--- packages/popup/src/pages/Error/index.js
+++ packages/popup/src/pages/Error/index.js
@@ -1,13 +1,16 @@
 /* TODO: Replace with real error page */
 import useGlobalStore from '../../stores/index.js'
+import {useQuery} from '../../hooks'
 
 function Error() {
   const {FATAL_ERROR} = useGlobalStore()
+  const query = useQuery()
+  const urlErrorMsg = query.get('errorMsg') ?? ''
 
   return (
     <div id="errorContainer">
       <h3>hoops.something goes wrong...</h3>
-      <p>{FATAL_ERROR}</p>
+      <p>{FATAL_ERROR || urlErrorMsg}</p>
     </div>
   )
 }
