7d5e4f3699cd817f6bd73a3a44c321ada6684130
RickStormBringer <13621099447@163.com>
2021-12-01 18:30:56 +0800

    feat: finish dom logic

--- packages/popup/src/pages/History/components/HistoryItem.js
+++ packages/popup/src/pages/History/components/HistoryItem.js
@@ -1,44 +1,139 @@
 import PropTypes from 'prop-types'
+import {shortenAddress} from '@fluent-wallet/shorten-address'
 import {
   CloseCircleFilled,
   ReloadOutlined,
   SendOutlined,
 } from '@fluent-wallet/component-icons'
 import {transformToTitleCase} from '../../../utils'
-import {WrapIcon, CopyButton} from '../../../components'
+import {
+  WrapIcon,
+  CopyButton,
+  DisplayBalance,
+  CustomTag,
+} from '../../../components'
 
 const tagColorStyle = {
   failed: 'bg-error-10 text-error',
-  executed: 'bg-warning-10 text-warning',
+  executed: 'bg-[#F0FDFC] text-[#83DBC6]',
   pending: 'bg-warning-10 text-warning',
 }
 
-function Tag({status}) {
-  const colorStyle = tagColorStyle[status] ?? ''
+function WrapperWithCircle({children, className}) {
   return (
-    <div className={`${colorStyle}`}>
-      <CloseCircleFilled />
-      <span>{transformToTitleCase(status)}</span>
+    <div
+      className={`${className} flex items-center justify-center	rounded-full w-3 h-3 mr-1`}
+    >
+      {children}
     </div>
   )
 }
 
-Tag.propTypes = {
-  status: PropTypes.string.isRequired,
+WrapperWithCircle.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node]).isRequired,
+  className: PropTypes.string,
 }
 
 function HistoryItem({itemData}) {
-  const {status, toAddress} = itemData
+  const {
+    status,
+    toAddress,
+    dappIcon,
+    methodName,
+    protocol,
+    dappUrl,
+    amount,
+    symbol,
+    isDapp,
+    nonce,
+    time,
+  } = itemData
+  const tagColor = tagColorStyle[status] ?? ''
+
   return (
-    <div>
-      <div>
-        <Tag status={status} />
-        <WrapIcon size="w-5 h-5" id="onCopyAddress" onClick={() => {}}>
-          <CopyButton text={toAddress} className="w-4 h-4 text-primary" />
-        </WrapIcon>
-        <WrapIcon size="w-5 h-5" id="openAddTokenBtn" onClick={() => {}}>
-          <SendOutlined className="w-4 h-4 text-primary" />
-        </WrapIcon>
+    <div className="px-3 pb-3 pt-2 relative bg-white mx-3 mt-3 rounded">
+      {isDapp ? (
+        <CustomTag
+          className={`${tagColor} absolute flex items-center h-6 px-2.5 left-0 top-0`}
+          width="w-auto"
+          roundedStyle="rounded-tl rounded-br-lg"
+        >
+          {status === 'failed' ? (
+            <CloseCircleFilled className="w-3 h-3 mr-1" />
+          ) : status === 'executed' ? (
+            <WrapperWithCircle className="bg-[#83DBC6]">
+              <ReloadOutlined className="w-2 h-2 text-white" />
+            </WrapperWithCircle>
+          ) : status === 'pending' ? (
+            <WrapperWithCircle className="bg-[#F0955F]">
+              <ReloadOutlined className="w-2 h-2 text-white" />
+            </WrapperWithCircle>
+          ) : null}
+          <span className="text-sm">{transformToTitleCase(status)}</span>
+        </CustomTag>
+      ) : null}
+
+      <div className="flex justify-between">
+        <div>
+          {status === 'completed' ? (
+            <div className="text-gray-60 text-xs">
+              <span>#{nonce}</span>
+              <span className="ml-2">{time}</span>
+            </div>
+          ) : null}
+        </div>
+        <div className="flex">
+          <CopyButton
+            text={toAddress}
+            className="w-3 h-3 text-primary"
+            CopyWrapper={WrapIcon}
+            wrapperClassName="!w-5 !h-5"
+          />
+          {/* TODO: jump scan */}
+          <WrapIcon size="w-5 h-5 ml-2" id="openScanUrl" onClick={() => {}}>
+            <SendOutlined className="w-3 h-3 text-primary" />
+          </WrapIcon>
+        </div>
+      </div>
+      <div className="mt-3 flex items-center">
+        <div
+          className={`${
+            !isDapp ? 'bg-success-10 border-success-10' : 'border-gray-20'
+          } w-8 h-8 rounded-full border-solid border flex items-center justify-center mr-2`}
+        >
+          {isDapp ? (
+            <img
+              src={dappIcon || '/images/default-dapp-icon.svg'}
+              alt="favicon"
+              className="w-4 h-4"
+            />
+          ) : (
+            <SendOutlined className="w-4 h-4 text-success" />
+          )}
+        </div>
+        <div className="flex-1">
+          <div className="flex items-center justify-between">
+            <div className="w-[120px] text-gray-80 text-sm whitespace-nowrap overflow-hidden overflow-ellipsis">
+              {methodName}
+            </div>
+            {amount ? (
+              <div className="flex">
+                <DisplayBalance
+                  balance={amount}
+                  maxWidth={114}
+                  maxWidthStyle="max-w-[114px]"
+                />
+                <span className="text-gray-60 ml-0.5">{symbol}</span>
+              </div>
+            ) : (
+              <span className="text-gray-40 text-xs">--</span>
+            )}
+          </div>
+          <div className="flex mt-0.5 items-center justify-between text-gray-40 text-xs">
+            <div>{isDapp ? protocol || dappUrl : symbol}</div>
+            <div>{shortenAddress(toAddress)}</div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- packages/popup/src/pages/History/index.js
+++ packages/popup/src/pages/History/index.js
@@ -1,33 +1,75 @@
 import {useTranslation} from 'react-i18next'
 import {TitleNav} from '../../components'
 import HistoryItem from './components/HistoryItem'
+import {useState, useEffect, useRef} from 'react'
 
 function History() {
   const {t} = useTranslation()
+  const historyRef = useRef(null)
+  // mock
+  const [txList, setTxList] = useState([])
+  useEffect(() => {
+    setTxList(new Array(6).fill(''))
+  }, [])
+
+  const onScroll = () => {
+    if (
+      historyRef.current.scrollHeight - historyRef.current.clientHeight <=
+      historyRef.current.scrollTop
+    ) {
+      setTxList([...txList.concat(new Array(6).fill(''))])
+    }
+  }
   return (
-    <div id="historyContainer" className="bg-bg h-full">
+    <div
+      id="historyContainer"
+      className="bg-bg h-full overflow-auto"
+      onScroll={onScroll}
+      ref={historyRef}
+    >
       <TitleNav title={t('activity')} />
       <main>
-        <HistoryItem
+        {txList.map((item, index) => (
+          <HistoryItem
+            key={index}
+            itemData={{
+              status: 'pending',
+              isDapp: true,
+              protocol: 'uniswap v2',
+              dappUrl: 'xxxx',
+              dappIcon: '',
+              scanUrl: 'https://testnet.confluxscan.io/',
+              methodName: 'transferFrom',
+              toAddress: 'cfxtest:aapg6k55852jv1z2rkmk8wtd2amn2tnw3amfnmyx9b',
+              amount: '-1000',
+              symbol: 'cfx',
+            }}
+          />
+        ))}
+        {/* <HistoryItem
           itemData={{
-            status: 'failed',
+            status: 'pending',
             isDapp: true,
             protocol: 'uniswap v2',
             dappUrl: 'xxxx',
+            dappIcon: '',
             scanUrl: 'https://testnet.confluxscan.io/',
             methodName: 'transferFrom',
             toAddress: 'cfxtest:aapg6k55852jv1z2rkmk8wtd2amn2tnw3amfnmyx9b',
-            amount: '1000',
+            amount: '-1000',
+            symbol: 'cfx',
           }}
         />
-        {/* <HistoryItem
+        <HistoryItem
           itemData={{
             status: 'completed',
             isDapp: false,
             scanUrl: 'https://testnet.confluxscan.io/',
             toAddress: 'cfxtest:aapg6k55852jv1z2rkmk8wtd2amn2tnw3amfnmyx9b',
-            amount: '1000',
             methodName: 'send',
+            nonce: '123231',
+            time: '2021/11/21',
+            symbol: 'CFX',
           }}
         /> */}
       </main>
