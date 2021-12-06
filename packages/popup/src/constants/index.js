7c29aff5bc13444175b3d40ec203ca17589e22d6
angelia <greenfish84@126.com>
2021-12-06 11:59:48 +0800

    fix: fix bugs

--- packages/popup/src/constants/index.js
+++ packages/popup/src/constants/index.js
@@ -23,8 +23,7 @@ export * as RPC_METHODS from './rpcMethods'
 export * as ROUTES from './route'
 export const LANGUAGES = []
 export const ANIMATE_DURING_TIME = 300
-export const PASSWORD_REG_EXP =
-  /^(?=.*\d)(?=.*[a-zA-Z])[\da-zA-Z~!@#$%^&*]{8,16}$/
+export const PASSWORD_REG_EXP = /^(?=.*\d)(?=.*[a-zA-Z]).{8,16}$/
 export const NETWORK_TYPE = {
   CFX: 'cfx',
   ETH: 'eth',
