2665a5421a91bf2d114b4d5ec0c2348add188d5e
SSSensational <weiwen.liu@confluxnetwork.org>
2021-12-19 16:51:31 +0800

    feat: add router level transition

--- packages/popup/src/App.js
+++ packages/popup/src/App.js
@@ -1,45 +1,49 @@
 import {ErrorBoundary} from 'react-error-boundary'
 import {isUndefined} from '@fluent-wallet/checks'
-import {lazy, Suspense} from 'react'
-import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
+import {Suspense, cloneElement, useState, useEffect} from 'react'
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  withRouter,
+} from 'react-router-dom'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import {useIsLocked, useIsZeroGroup, usePendingAuthReq} from './hooks/useApi'
 import {ProtectedRoute} from './components'
 import {ROUTES} from './constants'
+import './App.css'
 import useGlobalStore from './stores/index.js'
 
 import ErrorPage from './pages/Error'
-const HomePage = lazy(() => import('./pages/Home'))
-const ConfirmSeed = lazy(() => import('./pages/CreateSeed/ConfirmSeed'))
-const NewSeed = lazy(() => import('./pages/CreateSeed/NewSeed'))
-const Unlock = lazy(() => import('./pages/Unlock'))
-const Welcome = lazy(() => import('./pages/Welcome'))
-const SetPassword = lazy(() => import('./pages/SetPassword'))
-const SelectCreateType = lazy(() => import('./pages/SelectCreateType'))
-const ImportSeedPhrase = lazy(() => import('./pages/ImportSeedPhrase'))
-const ImportPrivateKey = lazy(() => import('./pages/ImportPrivateKey'))
-const BackupSeed = lazy(() => import('./pages/CreateSeed/BackupSeed'))
-const CurrentSeed = lazy(() => import('./pages/CurrentSeed'))
-const ConnectSite = lazy(() => import('./pages/ConnectSite'))
-const RequestSignature = lazy(() => import('./pages/RequestSignature'))
-const DappAddNetwork = lazy(() => import('./pages/DappAddNetwork'))
-const DappSwitchNetwork = lazy(() => import('./pages/DappSwitchNetwork'))
-const ConfirmAddSuggestedToken = lazy(() =>
-  import('./pages/ConfirmAddSuggestedToken'),
-)
-const SendTransaction = lazy(() => import('./pages/SendTransaction'))
-const EditGasFee = lazy(() => import('./pages/EditGasFee'))
-const EditPermission = lazy(() => import('./pages/EditPermission'))
-const ConfirmTransaction = lazy(() => import('./pages/ConfirmTransaction'))
-const History = lazy(() => import('./pages/History'))
-const ViewData = lazy(() => import('./pages/ViewData'))
-const AccountManagement = lazy(() => import('./pages/AccountManagement'))
-const ExportSeed = lazy(() => import('./pages/ExportSeed'))
-const ExportPrivateKey = lazy(() => import('./pages/ExportPrivateKey'))
-const HardwareGuard = lazy(() => import('./pages/HardwareGuard'))
-const ConnectHardwareWallet = lazy(() =>
-  import('./pages/ConnectHardwareWallet'),
-)
-const ImportHwAccount = lazy(() => import('./pages/ImportHwAccount'))
+import HomePage from './pages/Home'
+import ConfirmSeed from './pages/CreateSeed/ConfirmSeed'
+import NewSeed from './pages/CreateSeed/NewSeed'
+import Unlock from './pages/Unlock'
+import Welcome from './pages/Welcome'
+import SetPassword from './pages/SetPassword'
+import SelectCreateType from './pages/SelectCreateType'
+import ImportSeedPhrase from './pages/ImportSeedPhrase'
+import ImportPrivateKey from './pages/ImportPrivateKey'
+import BackupSeed from './pages/CreateSeed/BackupSeed'
+import CurrentSeed from './pages/CurrentSeed'
+import ConnectSite from './pages/ConnectSite'
+import RequestSignature from './pages/RequestSignature'
+import DappAddNetwork from './pages/DappAddNetwork'
+import DappSwitchNetwork from './pages/DappSwitchNetwork'
+import ConfirmAddSuggestedToken from './pages/ConfirmAddSuggestedToken'
+import SendTransaction from './pages/SendTransaction'
+import EditGasFee from './pages/EditGasFee'
+import EditPermission from './pages/EditPermission'
+import ConfirmTransaction from './pages/ConfirmTransaction'
+import History from './pages/History'
+import ViewData from './pages/ViewData'
+import AccountManagement from './pages/AccountManagement'
+import ExportSeed from './pages/ExportSeed'
+import ExportPrivateKey from './pages/ExportPrivateKey'
+import HardwareGuard from './pages/HardwareGuard';
+import ConnectHardwareWallet from './pages/ConnectHardwareWallet';
+import ImportHwAccount from './pages/ImportHwAccount';
 
 const {
   HOME,
@@ -73,6 +77,188 @@ const {
   IMPORT_HW_ACCOUNT,
 } = ROUTES
 
+const routes = [
+  {
+    path: WALLET_UNLOCK,
+    component: Unlock,
+  },
+  {
+    path: WELCOME,
+    component: Welcome,
+  },
+  {
+    path: CURRENT_SEED_PHRASE,
+    component: CurrentSeed,
+  },
+  {
+    path: NEW_SEED_PHRASE,
+    component: NewSeed,
+  },
+  {
+    path: BACKUP_SEED_PHRASE,
+    component: BackupSeed,
+  },
+  {
+    path: CONFIRM_SEED_PHRASE,
+    component: ConfirmSeed,
+  },
+  {
+    path: IMPORT_SEED_PHRASE,
+    component: ImportSeedPhrase,
+  },
+  {
+    path: WALLET_IMPORT_PRIVATE_KEY,
+    component: ImportPrivateKey,
+  },
+  {
+    path: SEND_TRANSACTION,
+    component: SendTransaction,
+  },
+  {
+    path: CONFIRM_TRANSACTION,
+    component: ConfirmTransaction,
+  },
+  {
+    path: EDIT_GAS_FEE,
+    component: EditGasFee,
+  },
+  {
+    path: EDIT_PERMISSION,
+    component: EditPermission,
+  },
+  {
+    path: SET_PASSWORD,
+    component: SetPassword,
+  },
+  {
+    path: SELECT_CREATE_TYPE,
+    component: SelectCreateType,
+  },
+  {
+    path: VIEW_DATA,
+    component: ViewData,
+  },
+  {
+    path: CONNECT_SITE,
+    component: ConnectSite,
+  },
+  {
+    path: CONFIRM_ADD_SUGGESTED_TOKEN,
+    component: ConfirmAddSuggestedToken,
+  },
+  {
+    path: REQUEST_SIGNATURE,
+    component: RequestSignature,
+  },
+  {
+    path: DAPP_SWITCH_NETWORK,
+    component: DappSwitchNetwork,
+  },
+  {
+    path: DAPP_ADD_NETWORK,
+    component: DappAddNetwork,
+  },
+  {
+    path: HISTORY,
+    component: History,
+  },
+  {
+    path: ACCOUNT_MANAGEMENT,
+    component: AccountManagement,
+  },
+  {
+    path: EXPORT_SEED,
+    component: ExportSeed,
+  },
+  {
+    path: EXPORT_PRIVATEKEY,
+    component: ExportPrivateKey,
+  },
+  {
+    path: ERROR,
+    component: ErrorPage,
+  }, {
+    path: HARDWARE_GUARD,
+    component: HardwareGuard,
+  }, {
+    path: CONNECT_HARDWARE_WALLET,
+    component: ConnectHardwareWallet,
+  }, {
+    path: IMPORT_HW_ACCOUNT,
+    component: ImportHwAccount,
+  }
+]
+
+const PageLoading = () => {
+  const [inDelay, setInDelay] = useState(true)
+  // 400ms内能结束的loading一闪即逝是不好的交互体验。
+  useEffect(() => {
+    const timer = setTimeout(() => setInDelay(false), 400)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (inDelay) return null
+  return (
+    <div className="h-150 w-93 m-auto light flex items-center justify-center">
+      loading...
+    </div>
+  )
+}
+
+const MyRoutes = withRouter(
+  ({lockedData, pendingAuthReq, zeroGroup, location, history}) => {
+    // 正常情况下路由的前进后退应用forward / back的左右滑转场。
+    // 完全切换至不相关的内容时: 如点开钱包的首屏；切换到锁定页。这种地方应采用另外的转场动画。
+    const fullSwitch =
+      location.pathname === WALLET_UNLOCK || history.length === 1
+
+    return (
+      <TransitionGroup
+        className="h-full"
+        childFactory={child =>
+          cloneElement(child, {
+            classNames: `router router-${
+              fullSwitch
+                ? 'full-switch'
+                : history.action === 'PUSH'
+                ? 'forward'
+                : 'back'
+            }`,
+          })
+        }
+      >
+        <CSSTransition
+          key={location.pathname}
+          timeout={!fullSwitch ? 380 : 620}
+          appear
+          in
+        >
+          <Switch location={location}>
+            <ProtectedRoute
+              key={HOME}
+              pendingAuthReq={pendingAuthReq}
+              hasAccount={!zeroGroup}
+              isLocked={!zeroGroup && lockedData}
+              exact
+              path={HOME}
+              component={HomePage}
+            />
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                exact
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+            <Route path="*" render={() => <Redirect to={ERROR} />} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )
+  },
+)
+
 function App() {
   const lockedData = useIsLocked()
   const zeroGroup = useIsZeroGroup()
@@ -83,100 +269,23 @@ function App() {
     isUndefined(zeroGroup) ||
     (!zeroGroup && !lockedData && isUndefined(pendingAuthReq))
   ) {
-    return <div>loading...</div>
+    return <PageLoading />
   }
+
   return (
     <Router>
       <ErrorBoundary
         FallbackComponent={ErrorPage}
         onError={error => setFatalError(error)}
       >
-        <Suspense
-          fallback={
-            <div className="w-full h-full flex items-center justify-center">
-              loading
+        <Suspense fallback={<PageLoading />}>
+            <div className="h-150 w-93 m-auto light relative overflow-hidden">
+              <MyRoutes
+                lockedData={lockedData}
+                zeroGroup={zeroGroup}
+                pendingAuthReq={pendingAuthReq}
+              />
             </div>
-          }
-        >
-          <Switch>
-            <Route exact path={WALLET_UNLOCK} component={Unlock} />
-            <Route exact path={WELCOME} component={Welcome} />
-            <ProtectedRoute
-              pendingAuthReq={pendingAuthReq}
-              hasAccount={!zeroGroup}
-              isLocked={!zeroGroup && lockedData}
-              exact
-              path={HOME}
-              component={HomePage}
-            />
-            <Route exact path={CURRENT_SEED_PHRASE} component={CurrentSeed} />
-            <Route exact path={NEW_SEED_PHRASE} component={NewSeed} />
-            <Route exact path={BACKUP_SEED_PHRASE} component={BackupSeed} />
-            <Route exact path={CONFIRM_SEED_PHRASE} component={ConfirmSeed} />
-            <Route
-              exact
-              path={IMPORT_SEED_PHRASE}
-              component={ImportSeedPhrase}
-            />
-            <Route
-              exact
-              path={WALLET_IMPORT_PRIVATE_KEY}
-              component={ImportPrivateKey}
-            />
-            <Route exact path={SEND_TRANSACTION} component={SendTransaction} />
-            <Route
-              exact
-              path={CONFIRM_TRANSACTION}
-              component={ConfirmTransaction}
-            />
-            <Route exact path={EDIT_GAS_FEE} component={EditGasFee} />
-            <Route exact path={EDIT_PERMISSION} component={EditPermission} />
-            <Route exact path={SET_PASSWORD} component={SetPassword} />
-            <Route
-              exact
-              path={SELECT_CREATE_TYPE}
-              component={SelectCreateType}
-            />
-            <Route exact path={VIEW_DATA} component={ViewData} />
-            <Route exact path={CONNECT_SITE} component={ConnectSite} />
-            <Route
-              exact
-              path={CONFIRM_ADD_SUGGESTED_TOKEN}
-              component={ConfirmAddSuggestedToken}
-            />
-            <Route
-              exact
-              path={REQUEST_SIGNATURE}
-              component={RequestSignature}
-            />
-            <Route
-              exact
-              path={DAPP_SWITCH_NETWORK}
-              component={DappSwitchNetwork}
-            />
-            <Route exact path={DAPP_ADD_NETWORK} component={DappAddNetwork} />
-            <Route exact path={HISTORY} component={History} />
-            <Route
-              exact
-              path={ACCOUNT_MANAGEMENT}
-              component={AccountManagement}
-            />
-            <Route exact path={EXPORT_SEED} component={ExportSeed} />
-            <Route
-              exact
-              path={EXPORT_PRIVATEKEY}
-              component={ExportPrivateKey}
-            />
-            <Route
-              exact
-              path={CONNECT_HARDWARE_WALLET}
-              component={ConnectHardwareWallet}
-            />
-            <Route exact path={HARDWARE_GUARD} component={HardwareGuard} />
-            <Route exact path={IMPORT_HW_ACCOUNT} component={ImportHwAccount} />
-            <Route exact path={ERROR} component={ErrorPage} />
-            <Route path="*" render={() => <Redirect to={ERROR} />} />
-          </Switch>
         </Suspense>
       </ErrorBoundary>
     </Router>
