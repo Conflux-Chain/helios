c3d16ee2d4f10e41e9745fa8707b324e00af14da
angelia <greenfish84@126.com>
2021-12-23 13:49:34 +0800

    fix: PORTAL-1624

--- packages/popup/src/pages/Home/components/CurrentDapp.js
+++ packages/popup/src/pages/Home/components/CurrentDapp.js
@@ -69,10 +69,10 @@ function CurrentDapp() {
           </div>
           <div className="flex flex-col flex-1 items-center">
             <div className="flex w-full items-center justify-between mb-0.5">
-              <span className="text-gray-80 font-medium inline-block">
+              <span className="text-gray-80 font-medium inline-block max-w-[180px] text-ellipsis">
                 {origin}
               </span>
-              <span className="text-gray-60 text-xs inline-block mb-1 max-w-[188px] text-ellipsis">
+              <span className="text-gray-60 text-xs inline-block mb-1 max-w-[100px] text-ellipsis">
                 {connectedNickname}
               </span>
             </div>
@@ -89,7 +89,7 @@ function CurrentDapp() {
               {!isConnectedCurrentAccount ? (
                 <span
                   id="setAuthModalShowBtn"
-                  className="text-primary text-xs cursor-pointer inline-block max-w-[188px] text-ellipsis"
+                  className="text-primary text-xs cursor-pointer inline-block max-w-[140px] text-ellipsis"
                   onClick={() => setAuthModalShow(true)}
                   aria-hidden="true"
                 >
