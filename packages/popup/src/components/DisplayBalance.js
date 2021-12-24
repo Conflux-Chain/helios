e4e63a1e4b4476f7d2a3803e4c7a8dfc93ecc643
angelia <greenfish84@126.com>
2021-12-24 12:03:33 +0800

    feat: PORTAL-1898

--- packages/popup/src/components/DisplayBalance.js
+++ packages/popup/src/components/DisplayBalance.js
@@ -1,12 +1,14 @@
 import {useRef} from 'react'
 import PropTypes from 'prop-types'
-import {useFontSize} from '../hooks'
 import {
   formatBalance,
   roundBalance,
   COMMON_DECIMALS,
+  Big,
 } from '@fluent-wallet/data-format'
 import {isHexPrefixed} from '@fluent-wallet/utils'
+import Tooltip from '@fluent-wallet/component-tooltip'
+import {useFontSize} from '../hooks'
 
 function DisplayBalance({
   maxWidth = 175,
@@ -19,11 +21,18 @@ function DisplayBalance({
   id,
 }) {
   let displayBalance
+  let showTooltip
   if (balance) {
     if (isHexPrefixed(balance)) {
       displayBalance = formatBalance(balance, decimals)
     } else {
-      displayBalance = roundBalance(balance)
+      const bigBalance = new Big(balance)
+      showTooltip = bigBalance.lt(Big(1e-6)) && !bigBalance.eq(0)
+      if (showTooltip) {
+        displayBalance = '0.000000...'
+      } else {
+        displayBalance = roundBalance(balance)
+      }
     }
   }
   const balanceRef = useRef()
@@ -34,8 +43,10 @@ function DisplayBalance({
       id={id}
       className={`text-gray-80 font-mono font-semibold whitespace-nowrap overflow-hidden overflow-ellipsis relative ${maxWidthStyle} ${className}`}
     >
-      <span ref={balanceRef}>{displayBalance}</span>
-      {symbol && ` ${symbol}`}
+      <Tooltip content={`${balance} ${symbol}`}>
+        <span ref={balanceRef}>{displayBalance}</span>
+        {symbol && ` ${symbol}`}
+      </Tooltip>
       <span ref={hiddenRef} className="invisible absolute left-0">
         {displayBalance}
       </span>
