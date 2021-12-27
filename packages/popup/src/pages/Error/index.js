a5ba42fb03139ca34847517eefe410a3c9db979a
RickStormBringer <13621099447@163.com>
2021-12-27 16:12:12 +0800

    feat: add 404 route style

--- packages/popup/src/pages/Error/index.js
+++ packages/popup/src/pages/Error/index.js
@@ -1,18 +1,60 @@
-/* TODO: Replace with real error page */
+import {useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+import {CloseCircleFilled} from '@fluent-wallet/component-icons'
+import Button from '@fluent-wallet/component-button'
+import {useTranslation} from 'react-i18next'
 import useGlobalStore from '../../stores/index.js'
 import {useQuery} from '../../hooks'
+import {RPC_METHODS, ROUTES} from '../../constants'
+import {request} from '../../utils'
+const {CFX_GET_STATUS} = RPC_METHODS
+const {HOME} = ROUTES
 
 function Error() {
+  const {t} = useTranslation()
+  const history = useHistory()
   const {FATAL_ERROR} = useGlobalStore()
   const query = useQuery()
   const urlErrorMsg = query.get('errorMsg') ?? ''
+  // type: route,fullNode,inner
+  const [errorType, setErrorType] = useState('route')
 
-  return (
-    <div id="errorContainer" className="h-full w-full">
-      <h3>hoops.something goes wrong...</h3>
-      <p>{FATAL_ERROR || urlErrorMsg}</p>
+  useEffect(() => {
+    if (!FATAL_ERROR && !urlErrorMsg) {
+      return setErrorType('route')
+    }
+
+    request(CFX_GET_STATUS)
+      .then(() => setErrorType('inner'))
+      .catch(() => {
+        request(CFX_GET_STATUS)
+          .then(() => setErrorType('inner'))
+          .catch(() => setErrorType('fullNode'))
+      })
+  }, [FATAL_ERROR, urlErrorMsg])
+
+  return errorType ? (
+    <div id="errorContainer" className="h-full w-full flex flex-col p-6">
+      <div className="flex-1 text-center">
+        <CloseCircleFilled className="text-error w-20 h-20 mt-[108px] mb-6 mx-auto" />
+        <p className="text-base font-medium text-black mb-2">
+          {t('errorTile')}
+        </p>
+        <p className="text-gray-60 text-sm">{t('routeError')}</p>
+      </div>
+
+      {/* <p>{FATAL_ERROR || urlErrorMsg}</p> */}
+      <div>
+        <Button
+          id="setPasswordFormBtn"
+          className="w-70 mt-4 mx-auto"
+          onClick={() => history.push(HOME)}
+        >
+          {t('back')}
+        </Button>
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default Error
