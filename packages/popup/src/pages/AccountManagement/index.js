e2963418c5b84ab71e006fcc12f92d2ee3f33dbf
tangxuezhi <0x74616e67@gmail.com>
2021-12-29 15:50:20 +0800

    fix: PORTAL-2145, ui check

--- packages/popup/src/pages/AccountManagement/index.js
+++ packages/popup/src/pages/AccountManagement/index.js
@@ -53,7 +53,7 @@ function AccountManagementItem({
       ))}
       {groupType === 'hd' ? (
         <div
-          className="mx-3 py-4 text-gray-60 text-xs border-t cursor-pointer hover:text-primary"
+          className="mx-3 py-4 text-gray-60 text-xs border-t border-gray-10 cursor-pointer hover:text-primary"
           onClick={() =>
             onOpenConfirmPassword &&
             onOpenConfirmPassword(WALLET_EXPORT_ACCOUNT_GROUP, accountGroupEid)
