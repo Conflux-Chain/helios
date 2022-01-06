bde3bed313fe56cb751a64e6574760c87c531f75
yqrashawn <namy.19@gmail.com>
2022-01-06 11:19:56 +0800

    feat: add shortcut for open large screen for debug

--- packages/webextension/index.js
+++ packages/webextension/index.js
@@ -4,5 +4,6 @@ export * as badge from './badge.js'
 export * as idle from './idle.js'
 export * as tab from './tab.js'
 export * as runtime from './runtime.js'
+export {default as commands} from './commands.js'
 import browser from 'webextension-polyfill'
 export {browser}
