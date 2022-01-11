8078efdb599f3a2f43e375a54ffa3e3f31d7800d
tangxuezhi <0x74616e67@gmail.com>
2022-01-11 10:57:21 +0800

    fix: PORTAL-2280, update TokenList component

--- packages/popup/src/components/TokenList.js
+++ packages/popup/src/components/TokenList.js
@@ -1,27 +1,36 @@
 import PropTypes from 'prop-types'
 import {TokenItem} from './'
+import {isArray} from '@fluent-wallet/checks'
 
-function TokenList({tokenList, onSelectToken}) {
-  return (
-    <div
-      className="flex flex-1 flex-col overflow-y-auto no-scroll pb-3"
-      id="tokenList"
-    >
-      {tokenList.map((token, index) => (
+function TokenList({tokenList, onSelectToken, children, contentClassName}) {
+  const child = isArray(tokenList)
+    ? tokenList.map((token, index) => (
         <TokenItem
           onSelect={onSelectToken}
           token={token}
           key={index}
           index={index}
         />
-      ))}
+      ))
+    : children
+
+  return (
+    <div className="flex flex-1 flex-col relative" id="tokenList">
+      <div
+        className={`overflow-y-auto no-scroll absolute left-0 right-0 top-0 bottom-0 ${contentClassName}`}
+      >
+        {child}
+      </div>
+      <div className="absolute left-0 right-0 bottom-0 h-6 bg-token-background rounded-b-xl" />
     </div>
   )
 }
 
 TokenList.propTypes = {
-  tokenList: PropTypes.array.isRequired,
+  tokenList: PropTypes.array,
   onSelectToken: PropTypes.func,
+  children: PropTypes.node,
+  contentClassName: PropTypes.string,
 }
 
 export default TokenList
--- packages/popup/src/pages/SendTransaction/components/TokenAndAmount.js
+++ packages/popup/src/pages/SendTransaction/components/TokenAndAmount.js
@@ -43,7 +43,6 @@ const ChooseTokenList = ({open, onClose, onSelectToken}) => {
         {t('tokenList')}
       </span>
       <TokenList tokenList={homeTokenList} onSelectToken={onSelectToken} />
-      <div className="absolute left-0 right-0 bottom-0 h-6 bg-token-background rounded-b-xl" />
     </div>
   )
   return (
