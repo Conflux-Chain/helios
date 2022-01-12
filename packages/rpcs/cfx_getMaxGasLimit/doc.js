e5fe318772b4e059cd7f8980a8d61c8555776f5b
yqrashawn <namy.19@gmail.com>
2022-01-12 14:49:03 +0800

    feat: new rpc method cfx_getMaxGasLimit
    fix cache of cfx_getBestBlockHash, cfx_getBlockByHash
    
    PORTAL-2353

---
+++ packages/rpcs/cfx_getMaxGasLimit/doc.js
@@ -0,0 +1,10 @@
+export default {
+  metadata: {
+    provider: true,
+  },
+  en: `Get the allowed of max gas limit of current Cofnlux network.
+
+Returns a decimal number in \`string\`.
+
+> Note: This rpc method is not standard. It's not providered by the fullnode.`,
+}
