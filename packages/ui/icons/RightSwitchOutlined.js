0394a5e8ae87870312e7f849ee6a454254e869cd
RickStormBringer <13621099447@163.com>
2022-01-17 18:19:14 +0800

    feat: finish logic

---
+++ packages/ui/icons/RightSwitchOutlined.js
@@ -0,0 +1,30 @@
+function RightSwitchOutlined(props) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        d="M8 5h8a7 7 0 110 14H8A7 7 0 118 5z"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16 15a3 3 0 110-6 3 3 0 010 6z"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default RightSwitchOutlined
