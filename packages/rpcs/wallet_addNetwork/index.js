e5ce610ee5f46974c280a3ab0dfafe63deea3f8e
yqrashawn <namy.19@gmail.com>
2022-01-26 14:10:55 +0800

    feat: add wallet_updateNetwork (#665)
    PORTAL-2566

--- packages/rpcs/wallet_addNetwork/index.js
+++ packages/rpcs/wallet_addNetwork/index.js
@@ -95,19 +95,36 @@ export const main = async ({
     iconUrls = [],
     hdPath,
   },
+  // only when called from wallet_updateNetwork
+  toUpdateNetwork,
 }) => {
   const [url] = rpcUrls
   const [explorerUrl] = blockExplorerUrls
   const [iconUrl] = iconUrls
+
+  // duplidate name
   const [dupNameNetwork] = getNetworkByName(name)
-  if (dupNameNetwork) throw InvalidParams('Duplicate network name')
+  if (
+    (!toUpdateNetwork && dupNameNetwork) ||
+    (toUpdateNetwork && toUpdateNetwork.eid !== dupNameNetwork.eid)
+  )
+    throw InvalidParams('Duplicate network name')
+
+  // duplicate rpc endpoint
   const [dupEndpointNetwork] = getNetworkByEndpoint(url)
-  if (dupEndpointNetwork)
+  if (
+    (!toUpdateNetwork && dupEndpointNetwork) ||
+    (toUpdateNetwork && toUpdateNetwork.eid !== dupEndpointNetwork.eid)
+  )
     throw InvalidParams(
       `Duplicate network endpoint with network ${dupEndpointNetwork.eid}`,
     )
+
+  // invalid hdPath id
   if (Number.isInteger(hdPath) && !getHdPathById(hdPath))
     throw InvalidParams(`Invalid hdPath id ${hdPath}`)
+
+  // chain id duplicate with builtin network
   const [dupChainIdBuiltInNetwork] = getNetwork({chainId, builtin: true})
   if (dupChainIdBuiltInNetwork)
     throw InvalidParams(`Duplicate chainId ${chainId} with builtin network`)
@@ -137,11 +154,9 @@ export const main = async ({
     hdPathId = await wallet_addHdPath({name: `${name}-hdPath`, value: hdPath})
   }
 
-  const {
-    tempids: {networkId},
-  } = t([
+  const upsertResult = t([
     {
-      eid: 'networkId',
+      eid: toUpdateNetwork?.eid || 'networkId',
       network: {
         isCustom: true,
         name,
@@ -155,23 +170,36 @@ export const main = async ({
         builtin: false,
       },
     },
-    explorerUrl && {eid: 'networkId', network: {scanUrl: explorerUrl}},
-    iconUrl && {eid: 'networkId', network: {icon: iconUrl}},
+    explorerUrl && {
+      eid: toUpdateNetwork?.eid || 'networkId',
+      network: {scanUrl: explorerUrl},
+    },
+    iconUrl && {
+      eid: toUpdateNetwork?.eid || 'networkId',
+      network: {icon: iconUrl},
+    },
   ])
 
   const groups = filterAccountGroupByNetworkType(networkType)
 
-  await Promise.all(
-    groups.map(({eid}) =>
-      wallet_createAddress({accountGroupId: eid, networkId}),
-    ),
-  )
+  if (!toUpdateNetwork) {
+    // create new network address for each group
+    await Promise.all(
+      groups.map(({eid}) =>
+        wallet_createAddress({
+          accountGroupId: eid,
+          networkId: upsertResult.tempids.networkId,
+        }),
+      ),
+    )
+  }
 
+  // discover new accounts for each hd group
   await Promise.all(
     groups
       .filter(({vault: {type}}) => type === 'hd')
       .map(({eid}) => wallet_discoverAccounts({accountGroupId: eid})),
   )
 
-  return networkId
+  return toUpdateNetwork?.eid || upsertResult.tempids.networkId
 }
