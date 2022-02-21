d092c8c0c1bbdbf30d875f04b8ac7758fbc89166
yqrashawn <namy.19@gmail.com>
2022-02-21 11:28:44 +0800

    chore: use babashka task for faster dev workflow (#747)

---
+++ scripts/x/shadow.clj
@@ -0,0 +1,54 @@
+(ns x.shadow
+  (:require
+   ;; [babashka.process :refer [process]]
+   [babashka.tasks :refer [shell]]
+   [cheshire.core :as json]
+   [babashka.fs :as fs]
+   [clojure.string :as s]
+   [taoensso.timbre :refer [warn]]
+   [clojure.java.shell :refer [sh]]))
+
+(defonce project-dir (-> *file* fs/parent fs/parent fs/parent))
+
+(defn- shadow-edn []
+  (-> project-dir
+      (fs/path "shadow-cljs.edn")
+      fs/file
+      slurp
+      read-string))
+
+(defn- get-shadow-cljs-command [args]
+  (let [command "npx shadow-cljs"]
+    (if (= (first args) "server")
+      (let [jack-in-deps
+            (-> (sh "emacsclient" "--eval" "cider-jack-in-dependencies")
+                :out
+                s/trim-newline
+                read-string)
+            lein-jack-in-deps
+            (-> (sh "emacsclient" "--eval" "cider-jack-in-lein-plugins")
+                :out
+                s/trim-newline
+                read-string)
+            deps
+            (concat jack-in-deps lein-jack-in-deps)
+            ;; => "npx shadow-cljs -d nrepl/nrepl:0.9.0-beta5
+            ;;                     -d refactor-nrepl/refactor-nrepl:3.0.0-alpha13
+            ;;                     -d cider/cider-nrepl:0.27.2 "
+            command
+            (reduce (fn [acc [library version]]
+                      (str acc " -d " library ":" version))
+                    command deps)]
+        command)
+      command)))
+
+(defn run [& args]
+  (let [args (or args ["server"])
+        shadow-cljs-command (get-shadow-cljs-command args)]
+    (println (s/join " " (concat [">" shadow-cljs-command] args)))
+    (apply shell shadow-cljs-command args)))
+
+(defn release-all []
+  (let [{:keys [builds]} (shadow-edn)
+        ids (remove #{:cards :browser-test :karma-test} (keys builds))]
+    (apply run "release" ids)))
