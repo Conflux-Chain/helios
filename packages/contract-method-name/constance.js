d6f89bd1a28f5159c13d231eb006c1578dfeed94
RickStormBringer <13621099447@163.com>
2022-03-11 16:34:25 +0800

    feat: add decode erc20 method and data

--- packages/contract-method-name/constance.js
+++ packages/contract-method-name/constance.js
@@ -4,11 +4,11 @@ export const ETH_FOUR_BYTE_DOMAIN = 'https://www.4byte.directory'
 
 // ETH endpoints
 export const ETH_ENDPOINT = {
-  Mainnet: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  Ropsten: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  Rinkeby: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  Kovan: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-  Goerli: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  1: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  3: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  4: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  42: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+  5: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
 }
 
 export const CFX_SCAN_DOMAINS = {
