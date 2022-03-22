c8d9f4111affaf1e116665cc11467e22a16b620b
yqrashawn <namy.19@gmail.com>
2022-03-22 19:03:46 +0800

    chore(pkg): upgrade js-conflux-sdk to 2.0.4 (#838)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/dapp-cross-space/src/manage/useEvm/index.tsx
+++
@@ -1,6 +0,0 @@
-export {
-  useEvmMappedAddressBalance,
-  EvmMappedAddressBalanceProvider,
-} from './useEvmMappedAddressBalance'
-export {trackEvmMappedAddressBalanceChangeOnce, addEVMChainToMetaMask} from './EvmManage'
-export {useIsSupportEvmSpace} from './useIsSupportEvmSpace'
