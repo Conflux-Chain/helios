429731c0ebd4fd390e98661ab8544fbcda8ea6ee
yqrashawn <namy.19@gmail.com>
2022-04-06 15:07:52 +0800

    fix: return msg of permission methods

--- packages/rpcs/wallet_getPermissions/index.js
+++ packages/rpcs/wallet_getPermissions/index.js
@@ -12,17 +12,28 @@ export const permissions = {
 
 export const main = ({Err: {InvalidRequest}, app}) => {
   if (!app) throw InvalidRequest()
-  const permissions = Object.keys(app.perms).reduce(
-    (acc, p) => [
-      ...acc,
+  const permissions = Object.keys(app.perms).reduce((acc, p) => {
+    const perm = [
       {
         invoker: app.site.origin,
         parentCapability: p,
         date: app.permUpdatedAt,
       },
-    ],
-    [],
-  )
+    ]
+    if (p.startsWith('wallet_')) {
+      perm.push({
+        invoker: app.site.origin,
+        parentCapability: p.replace('wallet_', 'cfx_'),
+        date: app.permUpdatedAt,
+      })
+      perm.push({
+        invoker: app.site.origin,
+        parentCapability: p.replace('wallet_', 'eth_'),
+        date: app.permUpdatedAt,
+      })
+    }
+    return acc.concat(perm)
+  }, [])
 
   return permissions
 }
