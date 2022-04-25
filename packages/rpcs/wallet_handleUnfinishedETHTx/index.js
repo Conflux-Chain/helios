021783de516f19fab3fc0fd341a7d97b125a9d38
yqrashawn <namy.19@gmail.com>
2022-04-25 08:00:20 +0800

    fix: tx error on espace

--- packages/rpcs/wallet_handleUnfinishedETHTx/index.js
+++ packages/rpcs/wallet_handleUnfinishedETHTx/index.js
@@ -239,7 +239,7 @@ export const main = ({
                           message: `Transaction ${parseInt(
                             tx.txPayload.nonce,
                             16,
-                          )} failed! ${err?.message || ''}`,
+                          )} failed! ${err?.data || err?.message || ''}`,
                         }),
                       )
                     }),
@@ -284,7 +284,7 @@ export const main = ({
               } else if (
                 BigNumber.from(n)
                   .sub(BigNumber.from(tx.resendAt || tx.blockNumber))
-                  .gte(10)
+                  .gte(40)
               ) {
                 setTxUnsent({hash, resendAt: n})
               }
