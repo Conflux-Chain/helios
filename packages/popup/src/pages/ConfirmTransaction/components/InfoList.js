179bbef238ded75981d45cd024c92fecbeeed1e4
Rick <13621099447@163.com>
2022-04-27 18:03:24 +0800

    fix: fix PORTAL-3049

--- packages/popup/src/pages/ConfirmTransaction/components/InfoList.js
+++ packages/popup/src/pages/ConfirmTransaction/components/InfoList.js
@@ -60,8 +60,8 @@ function InfoList({
             <DisplayBalance
               id="allowance"
               balance={customAllowance || allowance}
-              maxWidth={220}
-              maxWidthStyle="max-w-[220px]"
+              maxWidth={160}
+              maxWidthStyle="max-w-[160px]"
               symbol={token?.symbol}
             />
             <WrapIcon className="mx-1 shadow-none !bg-transparent hover:!bg-primary-4 group">
