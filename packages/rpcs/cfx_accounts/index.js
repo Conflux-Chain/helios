f3dc88f37ad616767e862aa34fc3a385f28f89f7
yqrashawn <namy.19@gmail.com>
2022-04-27 10:22:13 +0800

    feat: cross network type get address (#958)
    PORTAL-3030
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/rpcs/cfx_accounts/index.js
+++ packages/rpcs/cfx_accounts/index.js
@@ -1,31 +1,50 @@
-import {optParam} from '@fluent-wallet/spec'
+import {optParam, or, chainId, map} from '@fluent-wallet/spec'
 
 export const NAME = 'cfx_accounts'
 
 export const schemas = {
-  input: optParam,
+  input: [or, [map, {closed: true}, ['chainId', chainId]], optParam],
 }
 
 export const permissions = {
   external: ['popup', 'inpage'],
   locked: true,
-  db: ['getLocked', 'findAddress'],
+  db: ['getLocked', 'findAddress', 'getOneNetwork'],
 }
 
 export const main = async ({
-  db: {getLocked, findAddress},
+  db: {getLocked, findAddress, getOneNetwork},
   app,
   network,
   _inpage,
+  params,
 }) => {
   if (getLocked()) return []
   if (_inpage && !app) return []
+
+  if (
+    app &&
+    app.perms.wallet_crossNetworkTypeGetConfluxBase32Address &&
+    getOneNetwork({selected: true}).type !== 'cfx'
+  ) {
+    const addr = findAddress({
+      accountId: app.currentAccount.eid,
+      networkId: getOneNetwork({
+        type: 'cfx',
+        chainId: params?.chainId || '0x405',
+      }).eid,
+      g: {value: 1},
+    })
+    if (addr) return [addr.value]
+    else return []
+  }
 
   const addrs = findAddress({
     appId: app?.eid,
     networkId: app ? app.currentNetwork.eid : network.eid,
     g: {value: 1},
   })
+
   if (app) {
     return [addrs.value]
   }
