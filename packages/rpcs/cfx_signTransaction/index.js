3bd621b04101c6acd74ffe8c75fded0632741f39
Angelia <iangeliaq@gmail.com>
2022-05-19 14:27:11 +0800

    fix: estimate error (#1039)
    * fix: estimate error
    
    * fix: add blank line
    
    * chore: update version
    
    Co-authored-by: angelia <greenfish84@126.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/rpcs/cfx_signTransaction/index.js
+++ packages/rpcs/cfx_signTransaction/index.js
@@ -105,7 +105,6 @@ export const main = async args => {
 
   if (!newTx.chainId) newTx.chainId = network.chainId
   if (newTx.data === '0x') newTx.data = undefined
-  if (!newTx.gasPrice) newTx.gasPrice = await cfx_gasPrice()
 
   if (!newTx.value) newTx.value = '0x0'
 
@@ -140,6 +139,8 @@ export const main = async args => {
     if (!newTx.gas) newTx.gas = gasLimit
     if (!newTx.storageLimit) newTx.storageLimit = storageCollateralized
   }
+
+  if (!newTx.gasPrice) newTx.gasPrice = await cfx_gasPrice()
 
   let raw
   if (fromAddr.account.accountGroup.vault.type === 'hw') {
