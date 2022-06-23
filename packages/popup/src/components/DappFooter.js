b7c055ca9d202008088477d9996e63e86d7d3d50
Rick <13621099447@163..com>
2022-06-23 11:29:02 +0800

    fix: fix PORTAL-3287

--- packages/popup/src/components/DappFooter.js
+++ packages/popup/src/components/DappFooter.js
@@ -106,10 +106,6 @@ function DappFooter({
 
   const onConfirm = async () => {
     try {
-      if (!req?.method) {
-        return
-      }
-
       if (isHwAccount) {
         if (!ledgerBindingApi) {
           return
@@ -179,7 +175,7 @@ function DappFooter({
         id="confirmBtn"
         className="flex-1"
         onClick={onConfirm}
-        disabled={confirmDisabled}
+        disabled={confirmDisabled || !req?.method}
       >
         {confirmText}
       </Button>
