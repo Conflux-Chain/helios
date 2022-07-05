a3bb372ed339e63f069af166708a4d323a490348
Rick <13621099447@163..com>
2022-07-05 15:21:16 +0800

    refactor: refactor history item

---
+++ packages/popup/src/pages/History/components/TransitionDetail.js
@@ -0,0 +1,197 @@
+import PropTypes from 'prop-types'
+import {useTranslation} from 'react-i18next'
+import {shortenAddress} from '@fluent-wallet/shorten-address'
+import Tooltip from '@fluent-wallet/component-tooltip'
+import {formatHexToDecimal} from '@fluent-wallet/data-format'
+import {SendOutlined} from '@fluent-wallet/component-icons'
+
+import {transformToTitleCase, formatIntoChecksumAddress} from '../../../utils'
+import {SlideCard, CopyButton, WrapIcon} from '../../../components'
+import {HistoryStatusIcon, HistoryBalance, ResendButtons} from './'
+
+function TransitionDetail({
+  open = false,
+  isNegativeAmount = false,
+  onClose,
+  txStatus = '',
+  dappIconUrl = '',
+  app,
+  createdTime = '',
+  amount = '',
+  symbol = '',
+  receipt,
+  isExternalTx,
+  fromAddress = '',
+  toAddress = '',
+  statusIconColor = '',
+  copyButtonContainerClassName,
+  copyButtonToastClassName,
+  txFeeDrip = '0x0',
+  hash,
+  transactionUrl,
+  payload,
+  errorType,
+  onCancelPendingTx,
+  onSpeedupPendingTx,
+}) {
+  const {t} = useTranslation()
+
+  return (
+    <SlideCard
+      id="tx-detail"
+      cardClassName="pb-6"
+      open={open}
+      onClose={onClose}
+      height="h-auto"
+      cardTitle={
+        <div className="flex items-center">
+          <HistoryStatusIcon
+            txStatus={txStatus}
+            dappIconUrl={dappIconUrl}
+            isDapp={!!app}
+            className={statusIconColor}
+          />
+          <div className="ml-2">
+            <div className="text-gray-80 font-medium">
+              {transformToTitleCase(txStatus)}
+            </div>
+            {txStatus === 'confirmed' && (
+              <div className="text-xs text-gray-40 mt-0.5">{createdTime}</div>
+            )}
+          </div>
+        </div>
+      }
+      cardContent={
+        <div className="bg-white p-3 mt-3">
+          {amount && (
+            <div>
+              <p className="text-gray-40 text-xs">{t('amount')}</p>
+              <HistoryBalance
+                showNegative={isNegativeAmount}
+                amount={amount}
+                symbol={symbol}
+                balanceFontSize={24}
+                balanceMaxWidth={140}
+                symbolClassName="text-2lg text-gray-80 ml-1 !text-gray-80 !font-bold"
+                className="text-2lg !font-bold"
+              />
+            </div>
+          )}
+
+          <div>
+            <p className="text-gray-40 text-xs mt-3">
+              {t(isExternalTx ? 'fromAddress' : 'toAddress')}
+            </p>
+            <div className="flex font-medium items-center">
+              <div>
+                {shortenAddress(
+                  formatIntoChecksumAddress(
+                    isExternalTx ? fromAddress : toAddress,
+                  ),
+                )}
+              </div>
+              {
+                <CopyButton
+                  text={isExternalTx ? fromAddress : toAddress}
+                  className="w-3 h-3 text-primary"
+                  containerClassName={copyButtonContainerClassName}
+                  toastClassName={copyButtonToastClassName}
+                  wrapperClassName="!w-5 !h-5 ml-1"
+                />
+              }
+            </div>
+          </div>
+          {receipt && (
+            <div>
+              <p className="text-gray-40 text-xs mt-3">{t('gasFee')}</p>
+              <HistoryBalance
+                amount={txFeeDrip}
+                symbol={symbol}
+                symbolClassName="ml-1 !font-medium !text-gray-80"
+                className="!font-medium"
+              />
+            </div>
+          )}
+          <div>
+            <p className="text-gray-40 text-xs mt-3">{t('hash')}</p>
+            <div className="flex items-center font-medium">
+              <Tooltip content={hash || ''} placement="topLeft">
+                <div className="max-w-[100px] text-ellipsis">{hash}</div>
+              </Tooltip>
+
+              {hash && (
+                <CopyButton
+                  text={hash}
+                  className="w-3 h-3 text-primary"
+                  containerClassName={copyButtonContainerClassName}
+                  toastClassName={copyButtonToastClassName}
+                  wrapperClassName="!w-5 !h-5"
+                />
+              )}
+              {transactionUrl && (
+                <WrapIcon
+                  size="w-5 h-5 ml-2"
+                  id="openScanTxUrl"
+                  onClick={() => window.open(transactionUrl)}
+                >
+                  <SendOutlined className="w-3 h-3 text-primary" />
+                </WrapIcon>
+              )}
+            </div>
+          </div>
+          <div>
+            <p className="text-gray-40 text-xs mt-3">{t('nonce')}</p>
+            <div className="font-medium">
+              #{formatHexToDecimal(payload?.nonce)}
+            </div>
+          </div>
+
+          {txStatus === 'failed' && (
+            <p className="text-error text-xs mt-3">{t(errorType)}</p>
+          )}
+        </div>
+      }
+      cardFooter={
+        txStatus === 'pending' &&
+        !isExternalTx && (
+          <ResendButtons
+            onCancelPendingTx={onCancelPendingTx}
+            onSpeedupPendingTx={onSpeedupPendingTx}
+            className="mt-3"
+            blank="mr-3"
+            buttonClassName="bg-primary-10 text-primary"
+            buttonTextClassName="ml-1"
+          />
+        )
+      }
+    />
+  )
+}
+
+TransitionDetail.propTypes = {
+  open: PropTypes.bool,
+  isExternalTx: PropTypes.bool,
+  isNegativeAmount: PropTypes.bool,
+  onClose: PropTypes.func,
+  txStatus: PropTypes.string,
+  dappIconUrl: PropTypes.string,
+  statusIconColor: PropTypes.string,
+  app: PropTypes.oneOfType([PropTypes.oneOf([null]), PropTypes.object]),
+  createdTime: PropTypes.string,
+  amount: PropTypes.string,
+  symbol: PropTypes.string,
+  receipt: PropTypes.object,
+  fromAddress: PropTypes.string,
+  toAddress: PropTypes.string,
+  copyButtonContainerClassName: PropTypes.string,
+  copyButtonToastClassName: PropTypes.string,
+  txFeeDrip: PropTypes.string,
+  hash: PropTypes.string,
+  transactionUrl: PropTypes.string,
+  payload: PropTypes.object.isRequired,
+  errorType: PropTypes.string,
+  onCancelPendingTx: PropTypes.func,
+  onSpeedupPendingTx: PropTypes.func,
+}
+
+export default TransitionDetail
