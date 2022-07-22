b1c2b0329899fbc26e511ddd472f5ab1ae9d86a4
yqrashawn <namy.19@gmail.com>
2022-07-22 14:53:02 +0800

    feat: use scan openapi to get contract abi (#1226)
    PORTAL-3385

---
+++ packages/confluxscan-api/contract.js
@@ -0,0 +1,37 @@
+import {fetch} from './fetcher.js'
+import {getURL, isCoreNetworkId} from './utils.js'
+
+export async function abi(opts = {}) {
+  if (!Number.isInteger(opts.networkId))
+    throw new Error('invalid networkId, must be a integer')
+
+  if (isCoreNetworkId(opts.networkId)) return abiCoreSpace(opts)
+  return abiESpace(opts)
+}
+
+export async function abiCoreSpace({
+  networkId = undefined,
+  address = undefined,
+}) {
+  try {
+    const res = await fetch(getURL(networkId, 'contract', '/getabi'), {
+      address,
+    }).json()
+    if (res.code === 0) return JSON.parse(res.data)
+    throw new Error(res.message)
+  } catch (err) {
+    return
+  }
+}
+
+export async function abiESpace({networkId = undefined, address = undefined}) {
+  try {
+    const res = await fetch(
+      getURL(networkId, {module: 'contract', action: 'getabi', address}),
+    ).json()
+    if (res.status === '1') return JSON.parse(res.result)
+    throw new Error(res.message)
+  } catch (err) {
+    return
+  }
+}
