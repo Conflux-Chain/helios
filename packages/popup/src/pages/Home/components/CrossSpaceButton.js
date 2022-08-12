b48e63c1cf50756abfe81fd2ed00b9c518876ed3
Angelia <iangeliaq@gmail.com>
2022-08-12 16:17:17 +0800

    feat: add cross space button to jump public test url in conflux testnet

--- packages/popup/src/pages/Home/components/CrossSpaceButton.js
+++ packages/popup/src/pages/Home/components/CrossSpaceButton.js
@@ -1,9 +1,10 @@
+import PropTypes from 'prop-types'
 import {CrossOutlined} from '@fluent-wallet/component-icons'
 import Tooltip from '@fluent-wallet/component-tooltip'
 import {useTranslation} from 'react-i18next'
 import {WrapIcon} from '../../../components'
 
-function CrossSpaceButton() {
+function CrossSpaceButton({type}) {
   const {t} = useTranslation()
 
   return (
@@ -12,15 +13,25 @@ function CrossSpaceButton() {
         <CrossOutlined
           className="text-white transition-all duration-100 ease-in-out w-4 h-4 cursor-pointer"
           id="openCrossSpace"
-          onClick={() =>
-            window.open(
-              'https://confluxhub.io/espace-bridge/cross-space#source=fluent-wallet',
-            )
-          }
+          onClick={() => {
+            if (type === 'mainnet') {
+              window.open(
+                'https://confluxhub.io/espace-bridge/cross-space#source=fluent-wallet',
+              )
+            } else if (type === 'testnet') {
+              window.open(
+                'https://test.confluxhub.io/espace-bridge/cross-space#source=fluent-wallet',
+              )
+            }
+          }}
         />
       </WrapIcon>
     </Tooltip>
   )
+}
+
+CrossSpaceButton.propTypes = {
+  type: PropTypes.string,
 }
 
 export default CrossSpaceButton
