b8d7e33ecf0da7908a3083c7e34cd0c59ce430e2
Rick <13621099447@163..com>
2022-11-04 16:38:24 +0800

    feat: change rinkeby ropsten and kovan into coustom network

--- packages/db/src/main/cfxjs/db/migrations/m13.cljs
+++ packages/db/src/main/cfxjs/db/migrations/m13.cljs
@@ -1,7 +1,7 @@
 (ns cfxjs.db.migrations.m13
   (:require
    [cfxjs.db.datascript.core :as d]
-   ["@fluent-wallet/consts" :refer [ETH_GOERLI_CURRENCY_SYMBOL ETH_GOERLI_CURRENCY_NAME ETH_GOERLI_CHAINID ETH_SEPOLIA_EXPLORER_URL DEFAULT_CURRENCY_DECIMALS ETH_SEPOLIA_CURRENCY_SYMBOL ETH_SEPOLIA_CURRENCY_NAME ETH_SEPOLIA_NETID ETH_SEPOLIA_CHAINID ETH_SEPOLIA_RPC_ENDPOINT ETH_SEPOLIA_NAME CFX_MAINNET_RPC_ENDPOINT CFX_MAINNET_CHAINID CFX_ESPACE_MAINNET_RPC_ENDPOINT CFX_ESPACE_MAINNET_CHAINID CFX_TESTNET_RPC_ENDPOINT CFX_TESTNET_CHAINID CFX_ESPACE_TESTNET_RPC_ENDPOINT CFX_ESPACE_TESTNET_CHAINID]]
+   ["@fluent-wallet/consts" :refer [ETH_KOVAN_CHAINID ETH_RINKEBY_CHAINID ETH_ROPSTEN_CHAINID ETH_GOERLI_CURRENCY_SYMBOL ETH_GOERLI_CURRENCY_NAME ETH_GOERLI_CHAINID ETH_SEPOLIA_EXPLORER_URL DEFAULT_CURRENCY_DECIMALS ETH_SEPOLIA_CURRENCY_SYMBOL ETH_SEPOLIA_CURRENCY_NAME ETH_SEPOLIA_NETID ETH_SEPOLIA_CHAINID ETH_SEPOLIA_RPC_ENDPOINT ETH_SEPOLIA_NAME CFX_MAINNET_RPC_ENDPOINT CFX_MAINNET_CHAINID CFX_ESPACE_MAINNET_RPC_ENDPOINT CFX_ESPACE_MAINNET_CHAINID CFX_TESTNET_RPC_ENDPOINT CFX_TESTNET_CHAINID CFX_ESPACE_TESTNET_RPC_ENDPOINT CFX_ESPACE_TESTNET_CHAINID]]
    [cfxjs.db.migutil :refer [update-version-tx]]))
 
 (def id 13)
@@ -25,6 +25,9 @@
         cfx-espace-mainet-id (get-network-id old-db CFX_ESPACE_MAINNET_CHAINID "eth")
         cfx-espace-testnet-id (get-network-id old-db CFX_ESPACE_TESTNET_CHAINID "eth")
         eth-testnet-goerli-id (get-network-id old-db ETH_GOERLI_CHAINID "eth")
+        eth-testnet-ropsten-id (get-network-id old-db ETH_ROPSTEN_CHAINID "eth")
+        eth-testnet-rinkeby-id (get-network-id old-db ETH_RINKEBY_CHAINID "eth")
+        eth-testnet-kovan-id (get-network-id old-db ETH_KOVAN_CHAINID "eth")
         sepolia {:db/id                  -10
                  :network/name           ETH_SEPOLIA_NAME
                  :network/icon           "https://cdn.jsdelivr.net/gh/Conflux-Chain/helios@dev/packages/built-in-network-icons/Ethereum.svg"
@@ -40,7 +43,7 @@
                  :network/builtin        true
                  :network/scanUrl        ETH_SEPOLIA_EXPLORER_URL
                  :network/cacheTime      15000
-                 :network/balanceChecker "0x55abba8d669d60a10c104cc493ec5ef389ec92bb"
+                 :network/balanceChecker ""
                  :network/isTestnet      true
                  :network/gasBuffer      1.5}
 
@@ -48,6 +51,10 @@
              {:db/id cfx-testnet-id :network/endpoint CFX_TESTNET_RPC_ENDPOINT}
              {:db/id cfx-espace-mainet-id :network/endpoint CFX_ESPACE_MAINNET_RPC_ENDPOINT}
              {:db/id  cfx-espace-testnet-id :network/endpoint CFX_ESPACE_TESTNET_RPC_ENDPOINT}
+             {:db/id  eth-testnet-ropsten-id :network/isCustom true :network/builtin false}
+             {:db/id  eth-testnet-rinkeby-id :network/isCustom true :network/builtin false}
+             {:db/id  eth-testnet-kovan-id :network/isCustom true :network/builtin false}
+
              {:db/id  eth-testnet-goerli-id :network/ticker {:name     ETH_GOERLI_CURRENCY_NAME
                                                              :symbol   ETH_GOERLI_CURRENCY_SYMBOL
                                                              :decimals DEFAULT_CURRENCY_DECIMALS
