2045b86453cf91557b0a3200ffd1bc5f58a7c1cf
Rick <31155797+RickStormBringer@users.noreply.github.com>
2022-12-23 17:39:04 +0800

    feat: replace ns logo (#1351)

--- packages/popup/src/components/NsNameLabel.js
+++ packages/popup/src/components/NsNameLabel.js
@@ -2,9 +2,16 @@ import PropTypes from 'prop-types'
 import Tooltip from '@fluent-wallet/component-tooltip'
 import {useTranslation} from 'react-i18next'
 import {formatNsName} from '../utils'
+import {NETWORK_TYPE} from '../constants'
+import {useCurrentAddress} from '../hooks/useApi'
+
 function NsNameLabel({nsName = '', className = '', toolTipPlacement = 'top'}) {
   const {t} = useTranslation()
-
+  const {
+    data: {
+      network: {type},
+    },
+  } = useCurrentAddress()
   return (
     <Tooltip
       placement={toolTipPlacement}
@@ -16,7 +23,20 @@ function NsNameLabel({nsName = '', className = '', toolTipPlacement = 'top'}) {
       }
     >
       <div className={`flex items-center ${className}`}>
-        <img src="/images/ns-logo.svg" alt="group-icon" className="w-6 h-6" />
+        {type === NETWORK_TYPE.CFX && (
+          <img
+            src="/images/cns-logo.svg"
+            alt="group-icon"
+            className="w-6 h-6"
+          />
+        )}
+        {type === NETWORK_TYPE.ETH && (
+          <img
+            src="/images/ens-logo.svg"
+            alt="group-icon"
+            className="w-6 h-6"
+          />
+        )}
         <div className="ml-0.5 text-primary text-ellipsis max-w-[140px]">
           {formatNsName(nsName)}
         </div>
