a46a69c91036c774af61c46c1441b9fceb9e1a27
iosh <ioshx@foxmail.com>
2024-08-08 10:45:57 +0800

    chore: update env check

--- packages/inner-utils/env.js
+++ packages/inner-utils/env.js
@@ -4,6 +4,10 @@ export const MODE = import.meta.env.NODE_ENV
 export const IS_TEST_MODE = MODE === 'test'
 export const IS_DEV_MODE = MODE === 'development'
 export const IS_PROD_MODE = MODE === 'production'
-export const IS_CI = Boolean(import.meta.env.CI)
+export const IS_CI = Boolean(
+  typeof import.meta.env.CI === 'string'
+    ? import.meta.env.CI === 'true'
+    : import.meta.env.CI,
+)
 
 export const isManifestV3 = browser.runtime.getManifest().manifest_version === 3
