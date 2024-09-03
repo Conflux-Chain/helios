6fceddff7e45c61a350903166f626eb84d98af27
iosh <ioshx@foxmail.com>
2024-09-03 15:48:46 +0800

    chore: update cache-rpc

--- packages/cache-rpc/index.test.js
+++ packages/cache-rpc/index.test.js
@@ -1,11 +1,12 @@
-// eslint-disable-next-line no-unused-vars
-import { expect, describe, test, it, jest, afterAll, afterEach, beforeAll, beforeEach } from '@jest/globals' // prettier-ignore
-// eslint-disable-next-line no-unused-vars
+import {expect, describe, test, beforeEach, vi} from 'vitest'
+
 import waitForExpect from 'wait-for-expect'
 import {init} from './index.js'
 
-let params, get, set
-describe('@fluent-wallet/cache-rpc', function () {
+let params
+let get
+let set
+describe('@fluent-wallet/cache-rpc', () => {
   beforeEach(() => {
     const cacheMethods = init()
     get = cacheMethods.get
@@ -24,7 +25,7 @@ describe('@fluent-wallet/cache-rpc', function () {
       },
     }
   })
-  describe('init', function () {
+  describe('init', () => {
     test('basic ttl cache', async () => {
       set(params)
       expect(get(params)).toBe(params.res.result)
@@ -59,45 +60,45 @@ describe('@fluent-wallet/cache-rpc', function () {
       expect(get(params)).toBe(params.res.result)
     })
     test('beforeGet conf return false', () => {
-      params.conf.beforeGet = jest.fn(() => false)
+      params.conf.beforeGet = vi.fn(() => false)
       set(params)
       expect(get(params)).toBeUndefined()
       expect(params.conf.beforeGet).toBeCalledWith(params.req)
     })
     test('beforeGet conf return true', () => {
-      params.conf.beforeGet = jest.fn(() => true)
+      params.conf.beforeGet = vi.fn(() => true)
 
       set(params)
       expect(get(params)).toBe(params.res.result)
       expect(params.conf.beforeGet).toBeCalledWith(params.req)
     })
     test('afterGet conf', () => {
-      params.conf.afterGet = jest.fn(() => 'foo')
+      params.conf.afterGet = vi.fn(() => 'foo')
       set(params)
       expect(get(params)).toBe('foo')
       expect(params.conf.afterGet).toBeCalledWith(params.req, params.res.result)
     })
     test('beforeSet return false', () => {
-      params.conf.beforeSet = jest.fn(() => false)
+      params.conf.beforeSet = vi.fn(() => false)
       set(params)
       expect(params.conf.beforeSet).toBeCalledWith(params.req, params.res)
       expect(get(params)).toBeUndefined()
     })
     test('beforeSet return true', () => {
-      params.conf.beforeSet = jest.fn(() => true)
+      params.conf.beforeSet = vi.fn(() => true)
       set(params)
       expect(params.conf.beforeSet).toBeCalledWith(params.req, params.res)
       expect(get(params)).toBe(params.res.result)
     })
     test('afterSet conf', () => {
-      params.conf.afterSet = jest.fn(() => 'foo')
+      params.conf.afterSet = vi.fn(() => 'foo')
       set(params)
       expect(params.conf.afterSet).toBeCalledWith(set, params.req, params.res)
     })
 
     test('errors', () => {
       const {set} = init()
-      let params = {
+      const params = {
         req: {
           method: 'foo',
           params: ['bar'],
