3d6d1a01850e00255e7caa9afa698aebe8e47a92
iosh <ioshx@foxmail.com>
2024-09-14 17:18:38 +0800

    chore: update persist-db-to-ext-storage test

--- packages/background/src/persist-db-to-ext-storage.test.js
+++ packages/background/src/persist-db-to-ext-storage.test.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import {expect, describe, test, it, jest, afterAll, afterEach, beforeAll, beforeEach} from '@jest/globals' // prettier-ignore
+import {expect, describe, it, beforeEach} from 'vitest'
 import {persist} from './persist-db-to-ext-storage.js'
 import browser from 'webextension-polyfill'
 import {EXT_STORAGE} from '@fluent-wallet/consts'
