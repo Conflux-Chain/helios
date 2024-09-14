fa1adc27b85460101bd9f4208bd6951c07cf18dc
iosh <ioshx@foxmail.com>
2024-09-14 16:49:49 +0800

    chore: update wallet_importPrivateKey test

--- packages/rpcs/wallet_importPrivateKey/index.test.js
+++ packages/rpcs/wallet_importPrivateKey/index.test.js
@@ -1,11 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import {expect, describe, test, it, jest, afterAll, afterEach, beforeAll, beforeEach} from '@jest/globals' // prettier-ignore
+import {expect, describe, it, vi} from 'vitest'
 import {main, schemas} from './'
 import {validate} from '@fluent-wallet/spec'
 
-describe('wallet_importPrivateKey', function () {
-  describe('schemas', function () {
-    it('should validate the input data depends on schema.input', async function () {
+describe('wallet_importPrivateKey', () => {
+  describe('schemas', () => {
+    it('should validate the input data depends on schema.input', async () => {
       expect(
         validate(schemas.input, {
           privateKey:
@@ -25,16 +24,16 @@ describe('wallet_importPrivateKey', function () {
     })
   })
 
-  describe('main', function () {
+  describe('main', () => {
     const input = {
       params: {
         privateKey:
           '0x39bdd4a2ec600e1f8bc93538ee81d59b551fc31b34e07f163112b0a6a260139a',
         password: '12345678',
       },
-      rpcs: {wallet_addVault: jest.fn(() => 1)},
+      rpcs: {wallet_addVault: vi.fn(() => 1)},
     }
-    it('should call the wallet_addVault rpc with the right params', async function () {
+    it('should call the wallet_addVault rpc with the right params', async () => {
       await main(input)
       expect(input.rpcs.wallet_addVault).toBeCalledWith(input.params)
     })
