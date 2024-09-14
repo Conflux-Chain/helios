e2edf4686c9302a9106c09a04b404b9f7da64e97
iosh <ioshx@foxmail.com>
2024-09-14 16:52:10 +0800

    chore: update wallet_lock test

--- packages/rpcs/wallet_lock/index.test.js
+++ packages/rpcs/wallet_lock/index.test.js
@@ -1,11 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import {expect, describe, test, it, jest, afterAll, afterEach, beforeAll, beforeEach} from '@jest/globals' // prettier-ignore
+import {expect, describe, it, vi} from 'vitest'
 import {main} from './'
 
-describe('wallet_lock', function () {
-  describe('main', function () {
-    it('should set locked to true in db', async function () {
-      const params = {db: {setLocked: jest.fn(), findApp: jest.fn(() => [])}}
+describe('wallet_lock', () => {
+  describe('main', () => {
+    it('should set locked to true in db', async () => {
+      const params = {db: {setLocked: vi.fn(), findApp: jest.fn(() => [])}}
       main(params)
       expect(params.db.setLocked).toBeCalled()
     })
