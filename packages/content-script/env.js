6c12ef372bfb1cdb0e515f61a62a1d4036f2b165
mosshqq <145656181+mosshqq@users.noreply.github.com>
2025-06-03 18:00:53 +0800

    fix: firefox inpage inject (#1564)

--- packages/content-script/env.js
+++ packages/content-script/env.js
@@ -1,6 +1,3 @@
-import browser from 'webextension-polyfill'
-
-export const isManifestV3 = browser.runtime.getManifest().manifest_version === 3
 export const isScriptingApiSupported =
   window.chrome &&
   typeof chrome.scripting !== 'undefined' &&
