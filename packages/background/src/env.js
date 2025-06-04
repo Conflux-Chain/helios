997434eb618a63f5fca545e9456cf481f9ebff47
mosshqq <145656181+mosshqq@users.noreply.github.com>
2025-06-04 11:29:35 +0800

    Fix/firefox (#1566)
    * fix: window.chrome
    
    * feat: version

--- packages/background/src/env.js
+++ packages/background/src/env.js
@@ -2,6 +2,6 @@ import browser from 'webextension-polyfill'
 
 export const isManifestV3 = browser.runtime.getManifest().manifest_version === 3
 export const isScriptingApiSupported =
-  window.chrome &&
+  chrome &&
   typeof chrome.scripting !== 'undefined' &&
   typeof chrome.scripting.registerContentScripts === 'function'
