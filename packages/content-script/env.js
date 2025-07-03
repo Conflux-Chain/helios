dafbd83c7df27b38397cde69956b853d7ec04b70
mosshqq <145656181+mosshqq@users.noreply.github.com>
2025-07-03 17:10:55 +0800

    Fix/mv3 flag (#1572)
    * fix: use window.chrome
    
    * feat: version
    
    * fix: rename isScriptingApiSupported to isManifestV3
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/content-script/env.js
+++ packages/content-script/env.js
@@ -1,2 +1,2 @@
-export const isScriptingApiSupported =
-  chrome?.runtime?.getManifest()?.manifest_version === 3
+export const isManifestV3 =
+  window.chrome?.runtime?.getManifest()?.manifest_version === 3
