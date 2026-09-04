cc52a77c077a821777165dfc98c374281e8e0fce
iosh <ioshx@foxmail.com>
2026-09-04 17:33:27 +0800

    fix eSpace sponsored card styles

--- packages/popup/src/components/GasFee.js
+++ packages/popup/src/components/GasFee.js
@@ -4,11 +4,12 @@ import {useHistory} from 'react-router-dom'
 import Link from '@fluent-wallet/component-link'
 import {formatBalance, GWEI_DECIMALS} from '@fluent-wallet/data-format'
 import {RightOutlined} from '@fluent-wallet/component-icons'
-import {DisplayBalance, CustomTag} from '../components'
 import {useIsCfxChain, useCurrentTicker} from '../hooks/useApi'
 import {useCurrentTxStore} from '../hooks'
 import useDebouncedValue from '../hooks/useDebouncedValue'
 import {ROUTES} from '../constants'
+import DisplayBalance from './DisplayBalance'
+import GasFeeCard from './GasFeeCard'
 const {EDIT_GAS_FEE} = ROUTES
 
 function GasFee({
@@ -61,82 +62,75 @@ function GasFee({
   const hasExistingSponsorship = isBePayed && sponsoredFeeDrip !== '0x0'
   const isEditDisabled = editDisabled || !feeBalance || !displayGasPrice
 
+  const action = goEdit ? (
+    <span className="flex items-center">
+      <Link
+        onClick={() => history.push(EDIT_GAS_FEE)}
+        disabled={isEditDisabled}
+      >
+        {editLabel || (uses1559Fees ? t(gasLevel) : t('edit'))}
+        <RightOutlined
+          className={`ml-1 h-3 w-3 ${
+            editDisabled ? 'text-gray-40' : 'text-primary'
+          }`}
+        />
+      </Link>
+    </span>
+  ) : null
+
+  const statusTag =
+    sponsored || hasExistingSponsorship ? (
+      <span className="text-2xs text-white">
+        {sponsored || isBeAllPayed ? t('sponsored') : t('partSponsored')}
+      </span>
+    ) : null
+
   return (
-    <div className="gas-fee-container flex flex-col">
-      <header
-        className={`gas-fee-header flex items-center justify-between w-full text-gray-40 ${titleClassName}`}
-      >
-        {titleDes || t('gasFee')}
-        {goEdit && (
-          <span className="flex items-center">
-            <Link
-              onClick={() => history.push(EDIT_GAS_FEE)}
-              disabled={isEditDisabled}
-            >
-              {editLabel || (uses1559Fees ? t(gasLevel) : t('edit'))}
-              <RightOutlined
-                className={`ml-1 h-3 w-3 ${
-                  editDisabled ? 'text-gray-40' : 'text-primary'
-                }`}
-              />
-            </Link>
-          </span>
-        )}
-      </header>
-      <div
-        className={`gas-fee-body flex items-center bg-gray-4 border-gray-10 rounded px-2 py-3 relative ${contentClassName}`}
-        id="gasFeeContainer"
-      >
-        {prefix && <div className="shrink-0">{prefix}</div>}
-        <div className="min-w-0 flex-1 flex flex-col">
+    <GasFeeCard
+      title={titleDes || t('gasFee')}
+      action={action}
+      prefix={prefix}
+      suffix={suffix}
+      statusTag={statusTag}
+      titleClassName={titleClassName}
+      contentClassName={contentClassName}
+    >
+      <DisplayBalance
+        id="realPayedFee"
+        balance={feeBalance}
+        maxWidth={202}
+        maxWidthStyle="max-w-[202px]"
+        className={`text-lg mb-0.5 font-medium ${
+          sponsored ? '!text-gray-40 line-through' : ''
+        }`}
+        symbol={feeSymbol}
+        decimals={feeDecimals}
+        initialFontSize={20}
+      />
+
+      {!sponsored && hasExistingSponsorship && (
+        <div className="flex text-gray-40">
+          <span>{`${t('sponsored')}:`}&nbsp;</span>
           <DisplayBalance
-            id="realPayedFee"
-            balance={feeBalance}
-            maxWidth={202}
-            maxWidthStyle="max-w-[202px]"
-            className={`text-lg mb-0.5 font-medium ${
-              sponsored ? '!text-gray-40 line-through' : ''
-            }`}
-            symbol={feeSymbol}
-            decimals={feeDecimals}
-            initialFontSize={20}
+            id="sponsoredFee"
+            balance={sponsoredFeeDrip}
+            maxWidth={230}
+            maxWidthStyle="max-w-[230px]"
+            className="!text-gray-40 !font-normal mb-0.5"
+            symbol={symbol}
+            decimals={decimals}
           />
-          {!sponsored && hasExistingSponsorship && (
-            <div className="flex text-gray-40">
-              <span>{`${t('sponsored')}:`}&nbsp;</span>
-              <DisplayBalance
-                id="sponsoredFee"
-                balance={sponsoredFeeDrip}
-                maxWidth={230}
-                maxWidthStyle="max-w-[230px]"
-                className="!text-gray-40 !font-normal mb-0.5"
-                symbol={symbol}
-                decimals={decimals}
-              />
-            </div>
-          )}
-          {showDrip && (
-            <span className="text-xs text-gray-60">{`${formatBalance(
-              displayGasPrice,
-              GWEI_DECIMALS,
-            )} ${isCfxChain ? 'GDrip' : 'GWei'}`}</span>
-          )}
         </div>
-        {suffix && <div className="shrink-0">{suffix}</div>}
-        {(sponsored || hasExistingSponsorship) && (
-          <CustomTag
-            width="w-auto"
-            textColor="text-white"
-            backgroundColor="bg-[#44d7b6]"
-            className="absolute right-0 top-0 !h-6 px-2"
-          >
-            <span className="text-2xs text-white">
-              {sponsored || isBeAllPayed ? t('sponsored') : t('partSponsored')}
-            </span>
-          </CustomTag>
-        )}
-      </div>
-    </div>
+      )}
+
+      {showDrip && (
+        <span className="text-xs text-gray-60">
+          {`${formatBalance(displayGasPrice, GWEI_DECIMALS)} ${
+            isCfxChain ? 'GDrip' : 'GWei'
+          }`}
+        </span>
+      )}
+    </GasFeeCard>
   )
 }
 
--- packages/popup/src/pages/ConfirmTransaction/components/SponsoredGasFee.js
+++ packages/popup/src/pages/ConfirmTransaction/components/SponsoredGasFee.js
@@ -1,45 +1,80 @@
 import PropTypes from 'prop-types'
 import {useTranslation} from 'react-i18next'
-import {GasFee} from '../../../components'
+import {useHistory} from 'react-router-dom'
+import Link from '@fluent-wallet/component-link'
+import {RightOutlined} from '@fluent-wallet/component-icons'
+import DisplayBalance from '../../../components/DisplayBalance'
+import GasFeeCard from '../../../components/GasFeeCard'
 import {useCheckImage} from '../../../hooks'
+import {ROUTES} from '../../../constants'
 
-function SponsoredGasFee({maxGasCost, nativeToken, estimateRst, uses1559Fees}) {
+const {EDIT_GAS_FEE} = ROUTES
+
+function SponsoredGasFee({maxGasCost, nativeToken}) {
   const {t} = useTranslation()
+  const history = useHistory()
+
   const nativeTokenIcon =
     nativeToken?.logoURI || nativeToken?.icon || nativeToken?.iconUrls?.[0]
   const isImgUrl = useCheckImage(nativeTokenIcon)
 
+  // The sponsored UserOperation has no gas cost for the user.
+  const userGasCost = '0x0'
+
+  const action = (
+    <span className="flex items-center">
+      <Link onClick={() => history.push(EDIT_GAS_FEE)} disabled>
+        {t('medium')}
+        <RightOutlined className="ml-1 h-3 w-3 text-gray-40" />
+      </Link>
+    </span>
+  )
+
+  const statusTag = (
+    <span className="text-2xs text-white">{t('sponsored')}</span>
+  )
+
+  const tokenIcon = (
+    <div className="mr-2 flex items-center justify-center rounded-full bg-gray-0">
+      <img
+        src={isImgUrl ? nativeTokenIcon : '/images/default-token-icon.svg'}
+        alt=""
+        className="h-8 w-8 rounded-full"
+      />
+    </div>
+  )
+
   return (
-    <GasFee
-      estimateRst={estimateRst}
-      uses1559Fees={uses1559Fees}
-      showDrip={false}
-      displayFee={{
-        balance: maxGasCost,
-        symbol: nativeToken?.symbol,
-        decimals: nativeToken?.decimals,
-      }}
-      sponsored
-      editDisabled
-      editLabel={t('medium')}
-      prefix={
-        <div className="mr-2 flex items-center justify-center rounded-full bg-gray-0">
-          <img
-            src={isImgUrl ? nativeTokenIcon : '/images/default-token-icon.svg'}
-            alt=""
-            className="h-8 w-8 rounded-full"
-          />
-        </div>
-      }
-    />
+    <GasFeeCard
+      title={t('gasFee')}
+      action={action}
+      prefix={tokenIcon}
+      statusTag={statusTag}
+    >
+      <DisplayBalance
+        id="realPayedFee"
+        balance={userGasCost}
+        className="e-space-gas-fee-user-cost !font-body text-base !font-medium !text-gray-80"
+        symbol={nativeToken?.symbol}
+        decimals={nativeToken?.decimals}
+        initialFontSize={16}
+      />
+
+      <DisplayBalance
+        id="sponsoredFee"
+        balance={maxGasCost}
+        className="e-space-gas-fee-sponsored-cost !font-body text-sm !font-normal !text-gray-40 line-through"
+        symbol={nativeToken?.symbol}
+        decimals={nativeToken?.decimals}
+        initialFontSize={14}
+      />
+    </GasFeeCard>
   )
 }
 
 SponsoredGasFee.propTypes = {
   maxGasCost: PropTypes.string,
   nativeToken: PropTypes.object,
-  estimateRst: PropTypes.object,
-  uses1559Fees: PropTypes.bool,
 }
 
 export default SponsoredGasFee
